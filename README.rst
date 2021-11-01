1c5f1c21d41fcf08337c0be37ab0d2305cc1f8f2
grantbuster <grant.buster@nrel.gov>
2021-11-01 11:47:43 -0600

    added base repo files

---
+++ README.rst
@@ -0,0 +1,5 @@
+#################
+Welcome to SUP3R!
+#################
+
+SUP3R is software for Super Resolving Renewable Resource Data (SUP3R).
---
+++ setup.py
@@ -0,0 +1,74 @@
+"""
+setup.py
+"""
+import os
+from codecs import open
+from setuptools import setup, find_packages
+from setuptools.command.develop import develop
+from subprocess import check_call
+import shlex
+from warnings import warn
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
+    readme = f.read()
+
+with open(os.path.join(here, "requirements.txt")) as f:
+    install_requires = f.readlines()
+
+with open(os.path.join(here, "sup3r", "version.py"), encoding="utf-8") as f:
+    version = f.read()
+
+version = version.split('=')[-1].strip().strip('"').strip("'")
+
+
+class PostDevelopCommand(develop):
+    """
+    Class to run post setup commands
+    """
+
+    def run(self):
+        """
+        Run method that tries to install pre-commit hooks
+        """
+        try:
+            check_call(shlex.split("pre-commit install"))
+        except Exception as e:
+            warn("Unable to run 'pre-commit install': {}"
+                 .format(e))
+
+        develop.run(self)
+
+
+setup(
+    name="NREL-sup3r",
+    version=version,
+    description="Super Resolving Renewable Resource Data (sup3r)",
+    long_description=readme,
+    author="Ryan King",
+    author_email="ryan.king@nrel.gov",
+    url="https://github.com/NREL/sup3r",
+    packages=find_packages(),
+    package_dir={"sup3r": "sup3r"},
+    include_package_data=True,
+    license="BSD 3-Clause",
+    zip_safe=False,
+    keywords="sup3r",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    test_suite="tests",
+    python_requires='>=3.7',
+    install_requires=install_requires,
+    extras_require={
+        "dev": ["flake8", "pre-commit", "pylint"],
+    },
+    cmdclass={"develop": PostDevelopCommand},
+)
