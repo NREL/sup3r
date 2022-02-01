7c12ede2cf916151c955abd0780e7579da4b360e
bnb32 <brandon.benton@nrel.gov>
2022-02-01 13:25:00 -0800

    split up Sup3rData into DataHandler, Batch, and BatchHandler classes. BatchHandler implemented as an iterator. Added tests to check correct shape of arrays and correct spatial_coarsening.
    added make method to batch handler and extended tests. removed RI h5 file and added CO files.

--- sup3r/data_handling/preprocessing.py
+++ sup3r/data_handling/preprocessing.py
@@ -2,77 +2,54 @@
 """
 Sup3r preprocessing module.
 """
-import logging
 import xarray as xr
 import numpy as np
 import os
-import json
-import time
-import sys
 
-from rex import init_logger
-from rex.resource_extraction.resource_extraction import ResourceX
-from rex.utilities.loggers import create_dirs
 from rex import WindX
 from reV.utilities.exceptions import ConfigError
 from sup3r.utilities import utilities
-from sup3r.pipeline import Status
 from sup3r import __version__
 
 
-logger = logging.getLogger(__name__)
+class DataHandler:
+    """Sup3r data handling and extraction"""
 
+    def __init__(self, data_files, target,
+                 shape, features, max_delta=20):
+        """Data handling and extraction
 
-class Sup3rData:
-    """Sup3r data handling framework."""
-
-    def __init__(self, out_dir,
-                 make_out_dirs=True):
-        """
         Parameters
         ----------
-        out_dir : str
-            Project directory.
-        make_out_dirs : bool
-            Flag to make output directories for logs
+        data_files : list
+            list of file paths
+        target : tuple
+            (lat, lon) lower left corner of raster
+        shape : tuple
+            (rows, cols) grid size
+        features : list
+            list of features to extract
+        max_delta : int, optional
+            Optional maximum limit on the raster shape that is retrieved at
+            once. If shape is (20, 20) and max_delta=10, the full raseter will
+            be retrieved in four chunks of (10, 10). This helps adapt to
+            non-regular grids that curve over large distances, by default 20
         """
+        self.data_files = data_files
+        self.features = features
+        self.shape = shape
+        self.target = target
+        self.raster_index = None
+        self.max_delta = max_delta
+        self.data, self.lat_lon = self.extract_data()
 
-        self._out_dir = out_dir
-        self._log_dir = os.path.join(out_dir, 'logs/')
-
-        if make_out_dirs:
-            for d in [self._out_dir, self._log_dir]:
-                create_dirs(d)
-
-    @staticmethod
-    def _log_version():
-        """Check Sup3r and python version"""
-
-        logger.info(f'Running Sup3r version: {__version__}')
-        logger.info(f'Running python version: {sys.version_info}')
-
-        is_64bits = sys.maxsize > 2 ** 32
-        if is_64bits:
-            msg = f'Running on 64-bit python, sys.maxsize: {sys.maxsize}'
-            logger.info(msg)
-        else:
-            msg = f'Running on 32-bit python, sys.maxsize: {sys.maxsize}'
-            logger.warning(msg)
-
-    @classmethod
-    def _get_high_res_data(cls, data_files, target, shape, features):
-        """Concatenate files along time dimension
+    def extract_data(self):
+        """Building base 4D data array
 
         Parameters
         ----------
         data_files : list
             list of strings of file paths
-        target : tuple
-            lat lon tuple for lower left coordinate of raster
-        shape : tuple
-            grid size of raster
-        features : list
-            list of fields to extract from data
 
         Returns
         -------
@@ -86,67 +63,29 @@ class Sup3rData:
             lat (lon) first channel (second channel)
         """
 
-        raster_index = cls.get_raster_index(data_files[0],
-                                            target,
-                                            shape)
+        raster_index = self.get_raster_index(self.data_files[0],
+                                             self.target,
+                                             self.shape)
 
-        y, lat_lon = cls._get_file_data(data_files[0],
-                                        raster_index,
-                                        features)
-        if len(data_files) > 1:
-            for f in data_files[1:]:
-                tmp = cls._get_file_data(f, raster_index,
-                                         features,
-                                         get_coords=False)
+        y, lat_lon = self._get_file_data(self.data_files[0],
+                                         raster_index,
+                                         self.features)
+        if len(self.data_files) > 1:
+            for f in self.data_files[1:]:
+                tmp = self._get_file_data(f, raster_index,
+                                          self.features,
+                                          get_coords=False)
                 y = np.concatenate((y, tmp), axis=2)
+
+        y = utilities.transform_rotate_wind(y, lat_lon, self.features)
+
+        self.data = y
+        self.lat_lon = lat_lon
+        self.raster_index = raster_index
 
         return y, lat_lon
 
-    @classmethod
-    def reshape_data(cls, x, y, n_observations):
-        """Reshape high and low resolution
-        data so the first dimension of x and y
-        are the same - a requirement for batching
-
-        Parameters
-        ----------
-        x : np.ndarray
-            4D array of low resolution data
-            (spatial_1, spatial_2, temporal, features)
-        y : np.ndarray
-            4D array of high resolution data
-            (spatial_1, spatial_2, temporal, features)
-
-        Returns
-        -------
-        x : np.ndarray
-            5D array of low resolution data
-            (n_observations, spatial_1, spatial_2, temporal, features)
-        y : np.ndarray
-            5D array of high resolution data
-            (n_observations, spatial_1, spatial_2, temporal, features)
-        """
-
-        y = y.reshape((n_observations,
-                       y.shape[0],
-                       y.shape[1],
-                       -1, y.shape[3]))
-
-        msg = f'Reshaping high resolution data: {y.shape}'
-        logger.info(msg)
-
-        x = x.reshape((n_observations,
-                       x.shape[0],
-                       x.shape[1],
-                       -1, x.shape[3]))
-
-        msg = f'Reshaping low resolution data: {x.shape}'
-        logger.info(msg)
-
-        return x, y
-
-    @classmethod
-    def get_raster_index(cls, file_path, target, shape):
+    def get_raster_index(self, file_path, target, shape):
         """Get raster index for file data
 
         Parameters
@@ -167,11 +106,9 @@ class Sup3rData:
 
         _, file_ext = os.path.splitext(file_path)
         if file_ext == '.h5':
-            resourceX = ResourceX(file_path)
-            logger.info('Getting raster index. '
-                        f'target={target}, shape={shape}')
-            raster_index = resourceX.get_raster_index(target, shape,
-                                                      max_delta=20)
+            with WindX(file_path) as res:
+                raster_index = res.get_raster_index(target, shape,
+                                                    max_delta=self.max_delta)
 
         elif file_ext == '.nc':
             nc_file = xr.open_dataset(file_path)
@@ -185,100 +122,6 @@ class Sup3rData:
             raise ConfigError('Data must be either h5 or netcdf '
                               f'but received file extension: {file_ext}')
         return raster_index
-
-    @classmethod
-    def run_preprocessing(cls, out_dir, var_kwargs,
-                       factory_kwargs=None, log_level='DEBUG',
-                       log_file='preprocessing.log',
-                       job_name=None):
-        """Run preprocessing for preprocessing
-
-        Parameters
-        ----------
-        out_dir : str
-            Project directory.
-        var_kwargs : dict
-            Namespace of kwargs
-        factory_kwargs : dict | None
-            Optional namespace of kwargs
-        log_level : str | None
-            Logging level (DEBUG, INFO). If None,
-            no logging will be initialized.
-        log_file : str
-            File to log to. Will be put in output directory.
-        job_name : str
-            Optional name for pipeline and status identification.
-        """
-
-        t0 = time.time()
-
-        sup3r = cls(out_dir)
-        sup3r._init_loggers(log_file=log_file,
-                            log_level=log_level)
-
-        if isinstance(factory_kwargs, str):
-            factory_kwargs = json.loads(factory_kwargs)
-        if isinstance(var_kwargs, str):
-            var_kwargs = json.loads(var_kwargs)
-
-        logger.info('Initializing variables')
-
-        data_files = var_kwargs['data_files']
-        target = var_kwargs['target']
-        shape = var_kwargs['shape']
-        features = var_kwargs['features']
-        n_observations = var_kwargs.get('n_observations', 1)
-        spatial_res = var_kwargs.get('spatial_res', None)
-        temporal_res = var_kwargs.get('temporal_res', None)
-
-        if shape[0] % spatial_res != 0:
-            msg = 'spatial_res must evenly divide grid size. '
-            msg += f'Received spatial_res: {spatial_res} '
-            msg += f'with grid size: ({shape})'
-            raise ValueError(msg)
-
-        msg = 'Getting training data. '
-        msg += f'target={target}, '
-        msg += f'shape={shape}, '
-        msg += f'features={features}'
-
-        logger.info(msg)
-
-        y, lat_lon = sup3r._get_high_res_data(data_files, target,
-                                              shape, features)
-
-        if y.shape[2] % n_observations != 0:
-            msg = 'n_observations must evenly divide time '
-            msg += 'dimension. Received n_observations='
-            msg += f'{n_observations} and time_steps={y.shape[2]}'
-            raise ValueError(msg)
-
-        logger.info('Checking features for wind and transforming')
-
-        y = sup3r.transform_rotate_wind(y, lat_lon, features)
-
-        msg = 'Coarsening high resolution data: '
-        msg += f'spatial_res={spatial_res}, '
-        msg += f'temporal_res={temporal_res}'
-        logger.info(msg)
-
-        x, _ = utilities.get_coarse_data(y, lat_lon,
-                                         spatial_res,
-                                         temporal_res)
-
-        x, y = sup3r.reshape_data(x, y, n_observations)
-
-        logger.info('Finished getting training data')
-
-        if job_name is not None:
-            runtime = (time.time() - t0) / 60
-            status = {'out_dir': out_dir,
-                      'job_status': 'successful',
-                      'runtime': runtime}
-            Status.make_job_file(out_dir, 'preprocessing',
-                                 job_name, status)
-
-        return x, y
 
     @classmethod
     def _get_file_data(cls, file_path,
@@ -353,8 +196,6 @@ class Sup3rData:
             Only returned if get_coords=True
         """
 
-        logger.info(f'Opening data file: {file_path}')
-
         with WindX(file_path, hsds=False) as handle:
 
             data = np.zeros((raster_index.shape[0],
@@ -362,17 +203,12 @@ class Sup3rData:
                              len(handle.time_index),
                              len(features)), dtype=np.float32)
 
-            logger.info('Populating data array')
             for j, f in enumerate(features):
-
-                logger.info(f'Extracting {f} from file')
-
                 data[:, :, :, j] = np.transpose(handle[f, :, raster_index],
                                                 (1, 2, 0))
             if get_coords:
                 lat_lon = np.zeros((raster_index.shape[0],
                                     raster_index.shape[1], 2))
-                logger.info('Populating lat_lon array')
                 lat_lon = handle.lat_lon[raster_index]
 
         if get_coords:
@@ -410,8 +246,6 @@ class Sup3rData:
             Only returned if get_coords=True
         """
 
-        logger.info(f'Opening data file: {file_path}')
-
         handle = xr.open_dataset(file_path)
 
         data = np.zeros((raster_index[0][1] - raster_index[0][0],
@@ -419,11 +253,7 @@ class Sup3rData:
                          handle['Times'].shape[0],
                          len(features)), dtype=np.float32)
 
-        logger.info('Populating data array')
         for j, f in enumerate(features):
-
-            logger.info(f'Extracting {f} from file')
-
             if len(handle[f].shape) > 3:
                 if level_index is None:
                     level_index = 0
@@ -444,7 +274,6 @@ class Sup3rData:
             if get_coords:
                 lat_lon = np.zeros((raster_index.shape[0],
                                     raster_index.shape[1], 2))
-                logger.info('Populating lat_lon array')
                 lat_lon[:, :, 0] = \
                     handle['XLAT'][0, raster_index[0][0]:raster_index[0][1], 0]
                 lat_lon[:, :, 1] = \
@@ -456,94 +285,116 @@ class Sup3rData:
         else:
             return data
 
-    def _init_loggers(self, loggers=None,
-                      log_dir='./logs',
-                      log_file='sup3r.log',
-                      log_level='DEBUG',
-                      log_version=True,
-                      use_log_dir=True):
-        """Initialize sup3r loggers.
+
+class Batch:
+    """Batch of low_res and high_res data"""
+
+    def __init__(self, low_res, high_res):
+        """Stores low and high res data
+
         Parameters
         ----------
-        loggers : None | list | tuple
-            List of logger names to initialize. None defaults to all Sup3r
-            loggers.
-        log_file : str
-            Log file name. Will be placed in the sup3r out dir.
-        log_level : str | None
-            Logging level (DEBUG, INFO). If None, no logging will be
-            initialized.
-        date : None | datetime
-            Optional date to put in the log file name.
-        use_log_dir : bool
-            Flag to use the class log directory (self._log_dir = ./logs/)
+        low_res : np.ndarray
+            4D array (batch_size, spatial_1, spatial_2, features)
+        high_res : np.ndarray
+            4D array (batch_size, spatial_1, spatial_2, features)
+        """
+        self.low_res = low_res
+        self.high_res = high_res
+
+
+class SpatialBatchHandler:
+    """Sup3r spatial batch handling class"""
+
+    def __init__(self, data, batch_size, spatial_res=2):
+        """
+        Parameters
+        ----------
+        data : np.ndarray
+            4D array (spatial_1, spatial_2, temporal, features)
+        batch_size : int
+            size of batches along temporal dimension
+        spatial_res: int
+            factor by which to coarsen spatial dimensions
         """
 
-        if log_level in ('DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'):
+        self.data = data
+        self.batch_size = batch_size
+        self.spatial_res = spatial_res
+        self.batch_indices = self._get_batch_indices()
+        self.max = len(self.batch_indices)
+        self._i = 0
+        self.low_res = None
+        self.high_res = None
+        self.dataHandler = None
 
-            if loggers is None:
-                loggers = ('sup3r.sup3r', 'sup3r.preprocessing')
-
-            if log_file is not None and use_log_dir:
-                log_file = os.path.join(log_dir, log_file)
-                create_dirs(os.path.dirname(log_file))
-
-            for name in loggers:
-                init_logger(name, log_level=log_level,
-                            log_file=log_file)
-        if log_version:
-            self._log_version()
-
-    @staticmethod
-    def transform_rotate_wind(y, lat_lon, features):
-        """Transform windspeed/direction to
-        u and v and align u and v with grid
+    @classmethod
+    def make(cls, data_files, target,
+             shape, features, batch_size=8,
+             spatial_res=3, max_delta=20):
+        """Method to initialize both
+        data and batch handlers
 
         Parameters
         ----------
-        y : np.ndarray
-            4D array of high res data
-        lat_lon : np.ndarray
-            3D array of lat lon
+        data_files : list
+            list of file paths
+        target : tuple
+            (lat, lon) lower left corner of raster
+        shape : tuple
+            (rows, cols) grid size
         features : list
-            list of extracted features
+            list of features to extract
+        batch_size : int
+            size of batches along temporal dimension
+        spatial_res: int
+            factor by which to coarsen spatial dimensions
+        max_delta : int, optional
+            Optional maximum limit on the raster shape that is retrieved at
+            once. If shape is (20, 20) and max_delta=10, the full raseter will
+            be retrieved in four chunks of (10, 10). This helps adapt to
+            non-regular grids that curve over large distances, by default 20
 
         Returns
         -------
-        y : np.ndarray
-            4D array of high res data with
-            (windspeed, direction) -> (u, v)
+        batchHandler : SpatialBatchHandler
+            batchHandler with dataHandler attribute
         """
+        dataHandler = DataHandler(data_files, target,
+                                  shape, features,
+                                  max_delta=max_delta)
+        batchHandler = SpatialBatchHandler(dataHandler.data,
+                                           batch_size,
+                                           spatial_res)
+        batchHandler.dataHandler = dataHandler
+        return batchHandler
 
-        for i, f in enumerate(features):
-            if f.split('_')[0] == 'windspeed':
-                if len(f.split('_')) > 1:
-                    height = f.split('_')[1]
-                    msg = f'Converting windspeed/direction to u/v ({height})'
-                    j = features.index(f'winddirection_{height}')
-                    features[i] = f'U_{height}'
-                    features[j] = f'V_{height}'
-                else:
-                    msg = 'Converting windspeed/direction to u/v'
-                    j = features.index('winddirection')
-                    features[i] = 'U'
-                    features[j] = 'V'
+    def _get_batch_indices(self):
+        """Get batches of data along temporal dimension
+        """
+        all_indices = np.arange(self.data.shape[2])
+        shuffled = all_indices.copy()
+        np.random.shuffle(shuffled)
+        n_batches = int(np.ceil(len(shuffled) / self.batch_size))
+        self.batch_indices = np.array_split(shuffled, n_batches)
 
-                logger.info(msg)
+        return self.batch_indices
 
-                y = utilities.transform_wind(y, i, j)
+    def __iter__(self):
+        self._i = 0
+        return self
 
-            if features[i].split('_')[0] == 'U':
-                if len(features[i].split('_')) > 1:
-                    height = features[i].split('_')[1]
-                    j = features.index(f'V_{height}')
-                    msg = f'Aligning u/v to grid ({height})'
-                else:
-                    j = features.index('V')
-                    msg = 'Aligning u/v to grid'
-
-                logger.info(msg)
-
-                y = utilities.rotate_u_v(y, i, j, lat_lon)
-
-        return y
+    def __next__(self):
+        if self._i < self.max:
+            self.high_res = \
+                self.data[:, :,
+                          self.batch_indices[self._i], :]
+            self.low_res = utilities.spatial_coarsening(self.high_res,
+                                                        self.spatial_res)
+            self.high_res = self.high_res.transpose((2, 0, 1, 3))
+            self.low_res = self.low_res.transpose((2, 0, 1, 3))
+            batch = Batch(self.low_res, self.high_res)
+            self._i += 1
+            return batch
+        else:
+            raise StopIteration
--- sup3r/utilities/utilities.py
+++ sup3r/utilities/utilities.py
@@ -5,6 +5,53 @@ trraining data"""
 import numpy as np
 
 
+def transform_rotate_wind(y, lat_lon, features):
+    """Transform windspeed/direction to
+    u and v and align u and v with grid
+
+    Parameters
+    ----------
+    y : np.ndarray
+        4D array of high res data
+    lat_lon : np.ndarray
+        3D array of lat lon
+    features : list
+        list of extracted features
+
+    Returns
+    -------
+    y : np.ndarray
+        4D array of high res data with
+        (windspeed, direction) -> (u, v)
+    """
+
+    renamed_features = features.copy()
+    for i, f in enumerate(renamed_features):
+        if f.split('_')[0] == 'windspeed':
+            if len(f.split('_')) > 1:
+                height = f.split('_')[1]
+                j = renamed_features.index(f'winddirection_{height}')
+                renamed_features[i] = f'U_{height}'
+                renamed_features[j] = f'V_{height}'
+            else:
+                j = renamed_features.index('winddirection')
+                renamed_features[i] = 'U'
+                renamed_features[j] = 'V'
+
+            y = transform_wind(y, i, j)
+
+        if renamed_features[i].split('_')[0] == 'U':
+            if len(renamed_features[i].split('_')) > 1:
+                height = renamed_features[i].split('_')[1]
+                j = renamed_features.index(f'V_{height}')
+            else:
+                j = renamed_features.index('V')
+
+            y = rotate_u_v(y, i, j, lat_lon)
+
+    return y
+
+
 def transform_wind(y, i, j):
     """Maps windspeed and direction to u v
 
@@ -13,9 +60,9 @@ def transform_wind(y, i, j):
     y : np.ndarray
         4D array (spatial_1, spatial_2, temporal, features)
     i : int
-        index of windspeed feature
+        index of windspeed feature on the feature axis
     j : int
-        index of winddirection feature
+        index of winddirection feature on the feature axis
 
     Returns
     -------
@@ -43,9 +90,9 @@ def rotate_u_v(y, i, j, lat_lon):
     y : np.ndarray
         4D array (spatial_1, spatial_2, temporal, features)
     i : int
-        index of u feature
+        index of u feature along feature axis
     j : int
-        index of v feature
+        index of v feature along feature axis
     lat_lon : np.ndarray
         3D array (spatial_1, spatial_2, 2)
         2 channels are lat and lon in that
@@ -84,11 +131,35 @@ def rotate_u_v(y, i, j, lat_lon):
     return y
 
 
-def get_coarse_data(data, lat_lon,
-                    spatial_res=2,
-                    temporal_res=None):
+def temporal_coarsening(data, temporal_res=2):
+    """"Coarsen data according to temporal_res resolution
+
+    Parameters
+    ----------
+    data : np.ndarray
+        4D array with dimensions
+        (spatial_1, spatial_2, temporal, features)
+
+    temporal_res : int
+        factor by which to coarsen temporal dimension
+
+    Returns
+    -------
+    coarse_data : np.ndarray
+        4D array with same dimensions as data
+        with new coarse resolution
+    """
+
+    if temporal_res is not None:
+        coarse_data = data[:, :, ::temporal_res, :]
+    else:
+        coarse_data = data.copy()
+
+    return coarse_data
+
+
+def spatial_coarsening(data, spatial_res=2):
     """"Coarsen data according to spatial_res resolution
-    and temporal_res temporal sample frequency
 
     Parameters
     ----------
@@ -103,25 +174,12 @@ def get_coarse_data(data, lat_lon,
     spatial_res : int
         factor by which to coarsen spatial dimensions
 
-    temporal_res : (int, int)
-        factor by which to coarsen temporal dimension
-
     Returns
     -------
     coarse_data : np.ndarray
         4D array with same dimensions as data
         with new coarse resolution
-
-    coarse_lat_lon : np.ndarray
-        3D array (spatial_1, spatial_2, 2) with
-        lat and lon as the 2 channels in that order
-        with same resolution as coarse_data
     """
-
-    if temporal_res is not None:
-        tmp = data[:, :, ::temporal_res, :]
-    else:
-        tmp = data
 
     if spatial_res is not None:
         if data.shape[1] % spatial_res != 0:
@@ -131,19 +189,39 @@ def get_coarse_data(data, lat_lon,
             msg += f'{data.shape[1]})'
             raise ValueError(msg)
 
-        coarse_data = tmp.reshape(-1, spatial_res,
-                                  data.shape[1] // spatial_res,
-                                  spatial_res,
-                                  tmp.shape[2],
-                                  tmp.shape[3]).sum((1, 3)) \
+        coarse_data = data.reshape(-1, spatial_res,
+                                   data.shape[1] // spatial_res,
+                                   spatial_res,
+                                   data.shape[2],
+                                   data.shape[3]).sum((1, 3)) \
             / (spatial_res * spatial_res)
 
-        coarse_lat_lon = lat_lon.reshape(-1, spatial_res,
-                                         data.shape[1] // spatial_res,
-                                         spatial_res, 2).sum((3, 1)) \
-            / (spatial_res * spatial_res)
     else:
         coarse_data = data.copy()
-        coarse_lat_lon = lat_lon.copy()
 
-    return coarse_data, coarse_lat_lon
+    return coarse_data
+
+
+def lat_lon_coarsening(lat_lon, spatial_res=2):
+    """"Coarsen lat_lon according to spatial_res resolution
+
+    Parameters
+    ----------
+    lat_lon : np.ndarray
+        2D array with dimensions
+        (spatial_1, spatial_2)
+
+    spatial_res : int
+        factor by which to coarsen spatial dimensions
+
+    Returns
+    -------
+    coarse_lat_lon : np.ndarray
+        2D array with same dimensions as lat_lon
+        with new coarse resolution
+    """
+    coarse_lat_lon = lat_lon.reshape(-1, spatial_res,
+                                     lat_lon.shape[1] // spatial_res,
+                                     spatial_res, 2).sum((3, 1)) \
+        / (spatial_res * spatial_res)
+    return coarse_lat_lon
