e38e3b4b9accb85679942375e8173e37d2d8ac20
grantbuster <grant.buster@nrel.gov>
2022-02-02 11:48:35 -0700

    reorg sample configs and added tests for the spatial and spatiotemporal sample models

--- tests/test_spatial_model.py
+++ tests/test_spatial_model.py
@@ -1,27 +1,93 @@
 # -*- coding: utf-8 -*-
-"""Test the spatial super resolution GAN configs"""
+"""Test the sample super resolution GAN configs"""
 import os
 import json
 import numpy as np
+import tensorflow as tf
+
 from sup3r import CONFIG_DIR
-from phygnn.layers.handlers import HiddenLayers
-
-fp_gen = os.path.join(CONFIG_DIR, 'spatial/spatial_generator.json')
-fp_disc = os.path.join(CONFIG_DIR, 'spatial/spatial_discriminator.json')
-
-with open(fp_gen, 'r') as f:
-    GEN_CONFIG = json.load(f)
-
-with open(fp_disc, 'r') as f:
-    DISC_CONFIG = json.load(f)
+from sup3r.models.models import SpatialGan, SpatioTemporalGan
 
 
-if __name__ == '__main__':
+def test_load_spatial_gan():
+    """Test the loading of a sample the spatial gan model."""
+    fp_gen = os.path.join(CONFIG_DIR, 'spatial/gen_10x.json')
+    fp_disc = os.path.join(CONFIG_DIR, 'spatial/disc.json')
 
-    x = np.ones((32, 100, 100, 2))
+    with open(fp_gen, 'r') as f:
+        gen_config = json.load(f)
 
-    layers = HiddenLayers(DISC_CONFIG['hidden_layers'])
-    for i, layer in enumerate(layers):
+    with open(fp_disc, 'r') as f:
+        disc_config = json.load(f)
+
+    model = SpatialGan(gen_config['hidden_layers'],
+                       disc_config['hidden_layers'])
+
+    coarse_shape = (32, 5, 5, 2)
+    coarse_input = np.ones(coarse_shape)
+    x = np.ones(coarse_shape)
+
+    for layer in model.generator:
         x = layer(x)
-        print(i, layer, x.shape)
-    print(layers)
+
+    gen_out = tf.identity(x)
+
+    assert len(gen_out.shape) == 4
+    assert gen_out.shape[0] == coarse_input.shape[0]
+    assert gen_out.shape[1] == 10 * coarse_input.shape[1]
+    assert gen_out.shape[2] == 10 * coarse_input.shape[2]
+    assert gen_out.shape[3] == coarse_input.shape[3]
+
+    for layer in model.disc:
+        x = layer(x)
+
+    disc_out = tf.identity(x)
+    assert len(disc_out.shape) == 2
+    assert disc_out.shape[0] == coarse_input.shape[0]
+    assert disc_out.shape[1] == 1
+
+
+def test_load_spatiotemporal_gan():
+    """Test loading of a sample spatiotemporal gan model"""
+    fp_gen = os.path.join(CONFIG_DIR, 'spatiotemporal/gen_2x_24x.json')
+    fp_disc_s = os.path.join(CONFIG_DIR, 'spatiotemporal/disc_space.json')
+    fp_disc_t = os.path.join(CONFIG_DIR, 'spatiotemporal/disc_time.json')
+
+    with open(fp_gen, 'r') as f:
+        gen_config = json.load(f)
+
+    with open(fp_disc_s, 'r') as f:
+        disc_config_s = json.load(f)
+
+    with open(fp_disc_t, 'r') as f:
+        disc_config_t = json.load(f)
+
+    model = SpatioTemporalGan(gen_config['hidden_layers'],
+                              disc_config_t['hidden_layers'],
+                              disc_config_s['hidden_layers'])
+
+    coarse_shape = (32, 5, 5, 4, 2)
+    coarse_input = np.ones(coarse_shape)
+    x = np.ones(coarse_shape)
+
+    for layer in model.generator:
+        x = layer(x)
+
+    gen_out = tf.identity(x)
+
+    assert len(gen_out.shape) == 5
+    assert gen_out.shape[0] == coarse_input.shape[0]
+    assert gen_out.shape[1] == 2 * coarse_input.shape[1]
+    assert gen_out.shape[2] == 2 * coarse_input.shape[2]
+    assert gen_out.shape[3] == 24 * coarse_input.shape[3]
+    assert gen_out.shape[4] == coarse_input.shape[4]
+
+    for disc in (model.disc_spatial, model.disc_temporal):
+        x = tf.identity(gen_out)
+        for layer in disc:
+            x = layer(x)
+
+        disc_out = tf.identity(x)
+        assert len(disc_out.shape) == 2
+        assert disc_out.shape[0] == coarse_input.shape[0]
+        assert disc_out.shape[1] == 1
