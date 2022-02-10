390a40d27fd9da66f2dc2a64415af347af669891
grantbuster <grant.buster@nrel.gov>
2022-02-09 20:58:17 -0700

    added more simple config tests

--- tests/test_load_configs.py
+++ tests/test_load_configs.py
@@ -2,41 +2,38 @@
 """Test the sample super resolution GAN configs"""
 import os
 import numpy as np
+import pytest
 import tensorflow as tf
 
 from sup3r import CONFIG_DIR
 from sup3r.models.models import SpatialGan, SpatioTemporalGan
 
 
-def test_load_spatial():
+@pytest.mark.parametrize('spatial_len', (5, 6, 7))
+def test_load_spatial(spatial_len):
     """Test the loading of a sample the spatial gan model."""
     fp_gen = os.path.join(CONFIG_DIR, 'spatial/gen_10x.json')
     fp_disc = os.path.join(CONFIG_DIR, 'spatial/disc.json')
 
     model = SpatialGan(fp_gen, fp_disc)
 
-    coarse_shape = (32, 5, 5, 2)
-    coarse_input = np.ones(coarse_shape)
-    x = np.ones(coarse_shape)
+    coarse_shapes = [(32, spatial_len, spatial_len, 2),
+                     (16, 2 * spatial_len, 2 * spatial_len, 2)]
 
-    for layer in model.generator:
-        x = layer(x)
+    for coarse_shape in coarse_shapes:
+        x = np.ones(coarse_shape)
+        gen_out = model.generate(x)
 
-    gen_out = tf.identity(x)
+        assert len(gen_out.shape) == 4
+        assert gen_out.shape[0] == coarse_shape[0]
+        assert gen_out.shape[1] == 10 * coarse_shape[1]
+        assert gen_out.shape[2] == 10 * coarse_shape[2]
+        assert gen_out.shape[3] == coarse_shape[3]
 
-    assert len(gen_out.shape) == 4
-    assert gen_out.shape[0] == coarse_input.shape[0]
-    assert gen_out.shape[1] == 10 * coarse_input.shape[1]
-    assert gen_out.shape[2] == 10 * coarse_input.shape[2]
-    assert gen_out.shape[3] == coarse_input.shape[3]
-
-    for layer in model.disc:
-        x = layer(x)
-
-    disc_out = tf.identity(x)
-    assert len(disc_out.shape) == 2
-    assert disc_out.shape[0] == coarse_input.shape[0]
-    assert disc_out.shape[1] == 1
+        disc_out = model.discriminate(x)
+        assert len(disc_out.shape) == 2
+        assert disc_out.shape[0] == coarse_shape[0]
+        assert disc_out.shape[1] == 1
 
 
 def test_load_spatiotemporal():
@@ -48,7 +45,6 @@ def test_load_spatiotemporal():
     model = SpatioTemporalGan(fp_gen, fp_disc_s, fp_disc_t)
 
     coarse_shape = (32, 5, 5, 4, 2)
-    coarse_input = np.ones(coarse_shape)
     x = np.ones(coarse_shape)
 
     for layer in model.generator:
@@ -57,11 +53,11 @@ def test_load_spatiotemporal():
     gen_out = tf.identity(x)
 
     assert len(gen_out.shape) == 5
-    assert gen_out.shape[0] == coarse_input.shape[0]
-    assert gen_out.shape[1] == 2 * coarse_input.shape[1]
-    assert gen_out.shape[2] == 2 * coarse_input.shape[2]
-    assert gen_out.shape[3] == 24 * coarse_input.shape[3]
-    assert gen_out.shape[4] == coarse_input.shape[4]
+    assert gen_out.shape[0] == coarse_shape[0]
+    assert gen_out.shape[1] == 2 * coarse_shape[1]
+    assert gen_out.shape[2] == 2 * coarse_shape[2]
+    assert gen_out.shape[3] == 24 * coarse_shape[3]
+    assert gen_out.shape[4] == coarse_shape[4]
 
     for disc in (model.disc_spatial, model.disc_temporal):
         x = tf.identity(gen_out)
@@ -70,5 +66,5 @@ def test_load_spatiotemporal():
 
         disc_out = tf.identity(x)
         assert len(disc_out.shape) == 2
-        assert disc_out.shape[0] == coarse_input.shape[0]
+        assert disc_out.shape[0] == coarse_shape[0]
         assert disc_out.shape[1] == 1
