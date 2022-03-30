02d1ec15c1d11c108ed38a7e7a14fad35fd36ae0
grantbuster <grant.buster@nrel.gov>
2022-03-30 09:19:57 -0600

    added another test to make sure a trained st gan can generate using a new low res shape

--- tests/test_train_gan.py
+++ tests/test_train_gan.py
@@ -150,6 +150,15 @@ def test_train_st(n_epoch=6, log=False):
             loss_dummy = dummy.calc_loss(batch.high_res, out_dummy)[0]
             assert loss_og.numpy() < loss_dummy.numpy()
 
+        # test that a new shape can be passed through the generator
+        test_data = np.ones((3, 50, 50, 12, 2), dtype=np.float32)
+        y_test = model.generate(test_data)
+        assert y_test.shape[0] == test_data.shape[0]
+        assert y_test.shape[1] == test_data.shape[1] * 3
+        assert y_test.shape[2] == test_data.shape[2] * 3
+        assert y_test.shape[3] == test_data.shape[3] * 4
+        assert y_test.shape[4] == test_data.shape[4]
+
 
 def test_optimizer_update():
     """Test updating optimizer method."""
