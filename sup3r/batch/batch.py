e5cbe4082f587284df13670793c3a886b3afc38b
grantbuster <grant.buster@nrel.gov>
2022-08-25 10:49:48 -0700

    fixed sup3r pipeline object import

--- sup3r/batch/batch.py
+++ sup3r/batch/batch.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """sup3r batch utilities based on reV's batch module"""
-from sup3r.pipeline.pipeline import Pipeline
+from sup3r.pipeline.pipeline import Sup3rPipeline
 from sup3r.pipeline.pipeline_cli import pipeline_monitor_background
 from reV.batch.batch import BatchJob as RevBatchJob
 
@@ -12,5 +12,5 @@ class BatchJob(RevBatchJob):
     # background method. These can be updated in dependent software packages
     # for other workflows that want to utilize reV's pipeline and batch
     # features.
-    PIPELINE_CLASS = Pipeline
+    PIPELINE_CLASS = Sup3rPipeline
     PIPELINE_BACKGROUND_METHOD = pipeline_monitor_background
