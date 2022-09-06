started a huge bug fix with solar data handler for CC data from nsrdb files. Average of hourly clearsky ratios is not equivelant to the ratio of daily average ghi / clearsky ghi

sup3r/preprocessing/data_handling.py
@@ -1922,7 +1922,7 @@ class DataHandlerNCforCC(DataHandlerNC):
                                  len(cs_ghi) // (24 // time_freq))
         cs_ghi = [cs_ghi[window].mean(axis=0) for window in windows]
         cs_ghi = np.vstack(cs_ghi)
-        cs_ghi = cs_ghi.reshape((len(cs_ghi),) + self.grid_shape)
+        cs_ghi = cs_ghi.reshape((len(cs_ghi),) + tuple(self.grid_shape))
         cs_ghi = np.transpose(cs_ghi, axes=(1, 2, 0))
 
         if self.invert_lat:
@@ -2286,6 +2286,29 @@ class DataHandlerH5SolarCC(DataHandlerH5WindCC):
     # wildcard format.
     TRAIN_ONLY_FEATURES = ('U*', 'V*', 'topography')
 
+    def __init__(self, *args, **kwargs):
+        """
+        Parameters
+        ----------
+        *args : list
+            Same positional args as DataHandlerH5
+        **kwargs : dict
+            Same keyword args as DataHandlerH5
+        """
+
+        required = ['ghi', 'clearsky_ghi', 'clearsky_ratio']
+        missing = [dset for dset in required if dset not in args[1]]
+        if any(missing):
+            msg = ('Cannot initialize DataHandlerH5SolarCC without required '
+                   'features {}. All three are necessary to get the daily '
+                   'average clearsky ratio (ghi sum / clearsky ghi sum), even '
+                   'though only the clearsky ratio will be passed to the GAN.'
+                   .format(required))
+            logger.error(msg)
+            raise KeyError(msg)
+
+        super().__init__(*args, **kwargs)
+
     @classmethod
     def feature_registry(cls):
         """Registry of methods for computing features
@@ -2305,6 +2328,62 @@ class DataHandlerH5SolarCC(DataHandlerH5WindCC):
             'clearsky_ratio': ClearSkyRatioH5,
             'topography': TopoH5}
         return registry
+
+    def run_daily_averages(self):
+        """Calculate daily average data and store as attribute.
+
+        Note that the H5 clearsky ratio feature requires special logic to match
+        the climate change dataset of daily average GHI / daily average CS_GHI.
+        This target climate change dataset is not equivelant to the average of
+        instantaneous hourly clearsky ratios
+        """
+
+        msg = ('Data needs to be hourly with at least 24 hours, but data '
+               'shape is {}.'.format(self.data.shape))
+        assert self.data.shape[2] % 24 == 0, msg
+        assert self.data.shape[2] > 24, msg
+
+        n_data_days = int(self.data.shape[2] / 24)
+        daily_data_shape = (self.data.shape[0:2] + (n_data_days,)
+                            + (self.data.shape[3],))
+
+        logger.info('Calculating daily average datasets for {} training '
+                    'data days.'.format(n_data_days))
+
+        self.daily_data = np.zeros(daily_data_shape, dtype=np.float32)
+
+        self.daily_data_slices = np.array_split(np.arange(self.data.shape[2]),
+                                                n_data_days)
+        self.daily_data_slices = [slice(x[0], x[-1] + 1)
+                                  for x in self.daily_data_slices]
+
+        i_ghi = self.features.index('ghi')
+        i_cs = self.features.index('clearsky_ghi')
+        i_ratio = self.features.index('clearsky_ratio')
+
+        for d, t_slice in enumerate(self.daily_data_slices):
+            self.daily_data[:, :, d, i_ghi] = daily_temporal_coarsening(
+                self.data[:, :, t_slice, i_ghi], temporal_axis=2)[:, :, 0]
+            self.daily_data[:, :, d, i_cs] = daily_temporal_coarsening(
+                self.data[:, :, t_slice, i_cs], temporal_axis=2)[:, :, 0]
+
+            # note that this ratio of daily irradiance sums is not the same as
+            # the average of hourly ratios.
+            avg_cs_ratio = (self.data[:, :, t_slice, i_ghi].sum(axis=2)
+                            / self.data[:, :, t_slice, i_cs].sum(axis=2))
+            self.daily_data[:, :, d, i_ratio] = avg_cs_ratio
+
+        # remove ghi and clearsky ghi from feature set. These shouldn't be used
+        # downstream for solar cc and keeping them confuses the batch handler
+        ifeats = np.array([i for i in range(len(self.features))
+                           if i not in (i_ghi, i_cs)])
+        self.data = self.data[..., ifeats]
+        self.daily_data = self.daily_data[..., ifeats]
+        self.features.remove('ghi')
+        self.features.remove('clearsky_ghi')
+
+        logger.info('Finished calculating daily average datasets for {} '
+                    'training data days.'.format(n_data_days))
 
 
 # pylint: disable=W0223