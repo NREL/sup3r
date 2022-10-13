615170ec27e9eff45fc8ea71071e051ed99b11ab
bnb32 <brandon.benton@nrel.gov>
2022-10-12 19:12:08 -0600

    non cc wind model with hi res topo test

--- tests/test_train_wind.py
+++ tests/test_train_wind.py
@@ -11,9 +11,11 @@ from rex import init_logger
 from sup3r import TEST_DATA_DIR
 from sup3r import CONFIG_DIR
 from sup3r.models import WindGan
-from sup3r.preprocessing.data_handling import DataHandlerH5WindCC
+from sup3r.preprocessing.data_handling import (DataHandlerH5WindCC,
+                                               DataHandlerH5)
 from sup3r.preprocessing.batch_handling import (BatchHandlerCC,
-                                                SpatialBatchHandlerCC)
+                                                SpatialBatchHandlerCC,
+                                                SpatialBatchHandler)
 
 
 SHAPE = (20, 20)
@@ -25,6 +27,9 @@ TARGET_S = (39.01, -105.13)
 INPUT_FILE_W = os.path.join(TEST_DATA_DIR, 'test_wtk_co_2012.h5')
 FEATURES_W = ['U_100m', 'V_100m', 'temperature_100m', 'topography']
 TARGET_W = (39.01, -105.15)
+
+FP_WTK = os.path.join(TEST_DATA_DIR, 'test_wtk_co_2012.h5')
+TARGET_COORD = (39.01, -105.15)
 
 
 def test_wind_cc_model(log=False):
@@ -141,6 +146,91 @@ def test_wind_hi_res_topo(custom_layer, log=False):
 
     batcher = SpatialBatchHandlerCC([handler], batch_size=8, n_batches=10,
                                     s_enhance=2)
+
+    if log:
+        init_logger('sup3r', log_level='DEBUG')
+
+    gen_model = [{"class": "FlexiblePadding",
+                  "paddings": [[0, 0], [3, 3], [3, 3], [0, 0]],
+                  "mode": "REFLECT"},
+                 {"class": "Conv2DTranspose", "filters": 64, "kernel_size": 3,
+                  "strides": 1, "activation": "relu"},
+                 {"class": "Cropping2D", "cropping": 4},
+
+                 {"class": "FlexiblePadding",
+                  "paddings": [[0, 0], [3, 3], [3, 3], [0, 0]],
+                  "mode": "REFLECT"},
+                 {"class": "Conv2DTranspose", "filters": 64,
+                  "kernel_size": 3, "strides": 1, "activation": "relu"},
+                 {"class": "Cropping2D", "cropping": 4},
+
+                 {"class": "FlexiblePadding",
+                  "paddings": [[0, 0], [3, 3], [3, 3], [0, 0]],
+                  "mode": "REFLECT"},
+                 {"class": "Conv2DTranspose", "filters": 64,
+                  "kernel_size": 3, "strides": 1, "activation": "relu"},
+                 {"class": "Cropping2D", "cropping": 4},
+                 {"class": "SpatialExpansion", "spatial_mult": 2},
+                 {"class": "Activation", "activation": "relu"},
+
+                 {"class": custom_layer},
+
+                 {"class": "FlexiblePadding",
+                  "paddings": [[0, 0], [3, 3], [3, 3], [0, 0]],
+                  "mode": "REFLECT"},
+                 {"class": "Conv2DTranspose", "filters": 2,
+                  "kernel_size": 3, "strides": 1, "activation": "relu"},
+                 {"class": "Cropping2D", "cropping": 4}]
+
+    fp_disc = os.path.join(CONFIG_DIR, 'spatial/disc.json')
+
+    WindGan.seed()
+    model = WindGan(gen_model, fp_disc, learning_rate=1e-4)
+
+    with tempfile.TemporaryDirectory() as td:
+        model.train(batcher, n_epoch=1,
+                    weight_gen_advers=0.0,
+                    train_gen=True, train_disc=False,
+                    checkpoint_int=None,
+                    out_dir=os.path.join(td, 'test_{epoch}'))
+
+        assert 'test_0' in os.listdir(td)
+        assert model.meta['output_features'] == ['U_100m', 'V_100m']
+        assert model.meta['class'] == 'WindGan'
+        assert 'topography' in batcher.output_features
+        assert 'topography' not in model.output_features
+
+    x = np.random.uniform(0, 1, (4, 30, 30, 3))
+    hi_res_topo = np.random.uniform(0, 1, (60, 60))
+
+    with pytest.raises(RuntimeError):
+        y = model.generate(x, exogenous_data=None)
+
+    y = model.generate(x, exogenous_data=(None, hi_res_topo))
+
+    assert y.shape[0] == x.shape[0]
+    assert y.shape[1] == x.shape[1] * 2
+    assert y.shape[2] == x.shape[2] * 2
+    assert y.shape[3] == x.shape[3] - 1
+
+
+@pytest.mark.parametrize('custom_layer', ['Sup3rAdder', 'Sup3rConcat'])
+def test_wind_non_cc_hi_res_topo(custom_layer, log=False):
+    """Test a special wind model for non cc with the custom Sup3rAdder or
+    Sup3rConcat layer that adds/concatenates hi-res topography in the middle of
+    the network."""
+
+    handler = DataHandlerH5(FP_WTK,
+                            ('U_100m', 'V_100m', 'topography'),
+                            target=TARGET_COORD, shape=SHAPE,
+                            temporal_slice=slice(None, None, 10),
+                            val_split=0.0,
+                            sample_shape=(20, 20),
+                            max_workers=1,
+                            train_only_features=tuple())
+
+    batcher = SpatialBatchHandler([handler], batch_size=8, n_batches=10,
+                                  s_enhance=2)
 
     if log:
         init_logger('sup3r', log_level='DEBUG')
