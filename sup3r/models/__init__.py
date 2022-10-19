9ba40b43623c697053b1094b4c5be3a75d32aa09
grantbuster <grant.buster@nrel.gov>
2022-10-19 12:25:48 -0600

    moved simple -> linear model

--- sup3r/models/__init__.py
+++ sup3r/models/__init__.py
@@ -7,6 +7,7 @@ from .data_centric import Sup3rGanDC
 from .multi_step import (MultiStepGan, SpatialThenTemporalGan,
                          MultiStepSurfaceMetGan, SolarMultiStepGan)
 from .surface import SurfaceSpatialMetModel
+from .linear import LinearInterp
 
 SPATIAL_FIRST_MODELS = (SpatialThenTemporalGan, MultiStepSurfaceMetGan,
                         SolarMultiStepGan)
