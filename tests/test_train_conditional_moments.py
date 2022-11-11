024abeedf4bdd46f5d42deadd137284a2f9b2f1d
Malik <malik.hassanaly@gmail.com>
2022-11-11 16:15:41 -0700

    linting

--- tests/test_train_conditional_moments.py
+++ tests/test_train_conditional_moments.py
@@ -15,7 +15,8 @@ from sup3r import CONFIG_DIR
 from sup3r.models import Sup3rCondMom
 from sup3r.preprocessing.data_handling import DataHandlerH5
 from sup3r.preprocessing.batch_handling import (BatchHandler,
-                                                SpatialBatchHandler)
+                                                SpatialBatchHandler,
+                                                SpatialBatchHandler_mom2)
 
 
 FP_WTK = os.path.join(TEST_DATA_DIR, 'test_wtk_co_2012.h5')
@@ -23,10 +24,10 @@ TARGET_COORD = (39.01, -105.15)
 FEATURES = ['U_100m', 'V_100m']
 
 
-def test_train_st(log=False, full_shape=(20, 20),
-                  sample_shape=(12, 12, 24), n_epoch=4,
-                  batch_size=4, n_batches=4,
-                  out_dir_root=None):
+def test_train_st_mom1(log=False, full_shape=(20, 20),
+                       sample_shape=(12, 12, 24), n_epoch=4,
+                       batch_size=4, n_batches=4,
+                       out_dir_root=None):
     """Test basic spatiotemporal model training."""
     if log:
         init_logger('sup3r', log_level='DEBUG')
@@ -55,10 +56,10 @@ def test_train_st(log=False, full_shape=(20, 20),
                     out_dir=os.path.join(out_dir_root, 'test_{epoch}'))
 
 
-def test_train_spatial(log=False, full_shape=(20, 20),
-                       sample_shape=(10, 10, 1), n_epoch=4,
-                       batch_size=4, n_batches=4,
-                       out_dir_root=None):
+def test_train_spatial_mom1(log=False, full_shape=(20, 20),
+                            sample_shape=(10, 10, 1), n_epoch=4,
+                            batch_size=4, n_batches=4,
+                            out_dir_root=None):
     """Test basic spatial model training."""
     if log:
         init_logger('sup3r', log_level='DEBUG')
@@ -123,10 +124,10 @@ def test_train_spatial(log=False, full_shape=(20, 20),
             assert loss_og.numpy() < loss_dummy.numpy()
 
 
-def test_out_spatial(plot=False, full_shape=(20, 20),
-                     sample_shape=(10, 10, 1),
-                     batch_size=4, n_batches=4,
-                     s_enhance=2, model_dir=None):
+def test_out_spatial_mom1(plot=False, full_shape=(20, 20),
+                          sample_shape=(10, 10, 1),
+                          batch_size=4, n_batches=4,
+                          s_enhance=2, model_dir=None):
     """Test basic spatial model outputing."""
     handler = DataHandlerH5(FP_WTK, FEATURES, target=TARGET_COORD,
                             shape=full_shape,
@@ -166,6 +167,7 @@ def test_out_spatial(plot=False, full_shape=(20, 20),
         os.makedirs(figureFolder, exist_ok=True)
         movieFolder = os.path.join(figureFolder, 'Movie')
         os.makedirs(movieFolder, exist_ok=True)
+        mom_name = r'$\mathbb{E}$(HR|LR)'
         n_snap = 0
         for p, batch in enumerate(batch_handler):
             out = model._tf_generate(batch.low_res).numpy()
@@ -181,7 +183,7 @@ def test_out_spatial(plot=False, full_shape=(20, 20),
                     [0, batch.high_res.shape[1]],
                     [0, batch.high_res.shape[2]],
                     ['U [m/s]', 'U [m/s]', 'U [m/s]'],
-                    ['LR', 'HR', r'$\mathbb{E}$(HR|LR)'],
+                    ['LR', 'HR', mom_name],
                     ['x [m]', 'x [m]', 'x [m]'],
                     ['y [m]', 'y [m]', 'y [m]'],
                     [np.amin(lr), np.amin(hr), np.amin(hr)],
@@ -198,16 +200,94 @@ def test_out_spatial(plot=False, full_shape=(20, 20),
                   fps=6)
 
 
+def test_train_spatial_mom2(log=False, full_shape=(20, 20),
+                            sample_shape=(10, 10, 1), n_epoch=4,
+                            batch_size=4, n_batches=4,
+                            out_dir_root=None,
+                            model_dir=None):
+    """Test basic spatial model training for second conditional moment"""
+    if log:
+        init_logger('sup3r', log_level='DEBUG')
+
+    fp_gen = os.path.join(CONFIG_DIR, 'spatial/gen_2x_2f.json')
+
+    Sup3rCondMom.seed()
+    model = Sup3rCondMom(fp_gen, learning_rate=5e-5)
+    model_mom1 = Sup3rCondMom(fp_gen, learning_rate=5e-5)
+    model_mom1_loaded = model_mom1.load(model_dir)
+
+    handler = DataHandlerH5(FP_WTK, FEATURES, target=TARGET_COORD,
+                            shape=full_shape,
+                            sample_shape=sample_shape,
+                            temporal_slice=slice(None, None, 10),
+                            val_split=0.005,
+                            max_workers=1)
+
+    batch_handler = SpatialBatchHandler_mom2([handler], batch_size=batch_size,
+                                             s_enhance=2, n_batches=n_batches,
+                                             model_mom1=model_mom1_loaded)
+
+    with tempfile.TemporaryDirectory() as td:
+        if out_dir_root is None:
+            out_dir_root = td
+        model.train(batch_handler, n_epoch=n_epoch,
+                    checkpoint_int=2,
+                    out_dir=os.path.join(out_dir_root, 'test_{epoch}'))
+
+        assert len(model.history) == n_epoch
+        vlossg = model.history['val_loss_gen'].values
+        tlossg = model.history['train_loss_gen'].values
+        assert np.sum(np.diff(vlossg)) < 0
+        assert np.sum(np.diff(tlossg)) < 0
+        assert 'test_0' in os.listdir(out_dir_root)
+        assert 'test_2' in os.listdir(out_dir_root)
+        assert 'model_gen.pkl' in os.listdir(out_dir_root + '/test_3')
+
+        # make an un-trained dummy model
+        dummy = Sup3rCondMom(fp_gen, learning_rate=2e-5)
+
+        # test save/load functionality
+        out_dir = os.path.join(out_dir_root, 'spatial_cond_mom')
+        model.save(out_dir)
+        loaded = model.load(out_dir)
+
+        assert isinstance(dummy.loss_fun, tf.keras.losses.MeanSquaredError)
+        assert isinstance(model.loss_fun, tf.keras.losses.MeanSquaredError)
+        assert isinstance(loaded.loss_fun, tf.keras.losses.MeanSquaredError)
+
+        for batch in batch_handler:
+            out_og = model._tf_generate(batch.low_res)
+            out_dummy = dummy._tf_generate(batch.low_res)
+            out_loaded = loaded._tf_generate(batch.low_res)
+
+            # make sure the loaded model generates the same data as the saved
+            # model but different than the dummy
+            tf.assert_equal(out_og, out_loaded)
+            with pytest.raises(InvalidArgumentError):
+                tf.assert_equal(out_og, out_dummy)
+
+            # make sure the trained model has less loss than dummy
+            loss_og = model.calc_loss(batch.high_res, out_og)[0]
+            loss_dummy = dummy.calc_loss(batch.high_res, out_dummy)[0]
+            assert loss_og.numpy() < loss_dummy.numpy()
+
+
 if __name__ == "__main__":
-    # test_train_st(n_epoch=4, log=True, full_shape=(20, 20),
+    # test_train_st_mom1(n_epoch=4, log=True, full_shape=(20, 20),
     #               batch_size=4, n_batches=20,
     #               out_dir_root='st_model')
-    # test_train_spatial(n_epoch=10, log=True, full_shape=(20, 20),
+    # test_train_spatial_mom1(n_epoch=10, log=True, full_shape=(20, 20),
     #                    sample_shape=(10, 10, 1),
     #                    batch_size=16, n_batches=100,
     #                    out_dir_root='s_model')
 
-    test_out_spatial(plot=True, full_shape=(20, 20),
-                     sample_shape=(10, 10, 1),
-                     batch_size=4, n_batches=4,
-                     s_enhance=2, model_dir='s_model_save/spatial_cond_mom')
+    # test_out_spatial_mom1(plot=True, full_shape=(20, 20),
+    #                  sample_shape=(10, 10, 1),
+    #                  batch_size=4, n_batches=4,
+    #                  s_enhance=2, model_dir='s_model_save/spatial_cond_mom')
+
+    test_train_spatial_mom2(n_epoch=10, log=True, full_shape=(20, 20),
+                            sample_shape=(10, 10, 1),
+                            batch_size=16, n_batches=100,
+                            out_dir_root='s_model',
+                            model_dir='s_model_save/spatial_cond_mom')
