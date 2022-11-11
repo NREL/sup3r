82c75f6cb2989169ab076a4ddec84f70afe2ee43
Malik <malik.hassanaly@gmail.com>
2022-11-11 10:20:41 -0700

    linting

--- tests/test_train_conditional_moments.py
+++ tests/test_train_conditional_moments.py
@@ -24,7 +24,9 @@ FEATURES = ['U_100m', 'V_100m']
 
 
 def test_train_st(log=False, full_shape=(20, 20),
-                  n_epoch=4):
+                  sample_shape=(12, 12, 24), n_epoch=4,
+                  batch_size=4, n_batches=4,
+                  out_dir_root=None):
     """Test basic spatiotemporal model training."""
     if log:
         init_logger('sup3r', log_level='DEBUG')
@@ -36,23 +38,27 @@ def test_train_st(log=False, full_shape=(20, 20),
 
     handler = DataHandlerH5(FP_WTK, FEATURES, target=TARGET_COORD,
                             shape=full_shape,
-                            sample_shape=(12, 12, 24),
+                            sample_shape=sample_shape,
                             temporal_slice=slice(None, None, 1),
                             val_split=0.005,
                             max_workers=1)
 
-    batch_handler = BatchHandler([handler], batch_size=4,
+    batch_handler = BatchHandler([handler], batch_size=batch_size,
                                  s_enhance=3, t_enhance=4,
-                                 n_batches=4)
+                                 n_batches=n_batches)
 
     with tempfile.TemporaryDirectory() as td:
+        if out_dir_root is None:
+            out_dir_root = td
         model.train(batch_handler, n_epoch=n_epoch,
                     checkpoint_int=10,
-                    out_dir=os.path.join(td, 'test_{epoch}'))
+                    out_dir=os.path.join(out_dir_root, 'test_{epoch}'))
 
 
 def test_train_spatial(log=False, full_shape=(20, 20),
-                       sample_shape=(10, 10, 1), n_epoch=4):
+                       sample_shape=(10, 10, 1), n_epoch=4,
+                       batch_size=4, n_batches=4,
+                       out_dir_root=None):
     """Test basic spatial model training."""
     if log:
         init_logger('sup3r', log_level='DEBUG')
@@ -69,28 +75,30 @@ def test_train_spatial(log=False, full_shape=(20, 20),
                             val_split=0.005,
                             max_workers=1)
 
-    batch_handler = SpatialBatchHandler([handler], batch_size=4,
-                                        s_enhance=2, n_batches=10)
+    batch_handler = SpatialBatchHandler([handler], batch_size=batch_size,
+                                        s_enhance=2, n_batches=n_batches)
 
     with tempfile.TemporaryDirectory() as td:
+        if out_dir_root is None:
+            out_dir_root = td
         model.train(batch_handler, n_epoch=n_epoch,
                     checkpoint_int=2,
-                    out_dir=os.path.join(td, 'test_{epoch}'))
+                    out_dir=os.path.join(out_dir_root, 'test_{epoch}'))
 
         assert len(model.history) == n_epoch
         vlossg = model.history['val_loss_gen'].values
         tlossg = model.history['train_loss_gen'].values
         assert np.sum(np.diff(vlossg)) < 0
         assert np.sum(np.diff(tlossg)) < 0
-        assert 'test_0' in os.listdir(td)
-        assert 'test_2' in os.listdir(td)
-        assert 'model_gen.pkl' in os.listdir(td + '/test_3')
+        assert 'test_0' in os.listdir(out_dir_root)
+        assert 'test_2' in os.listdir(out_dir_root)
+        assert 'model_gen.pkl' in os.listdir(out_dir_root + '/test_3')
 
         # make an un-trained dummy model
         dummy = Sup3r_cond_mom(fp_gen, learning_rate=2e-5)
 
         # test save/load functionality
-        out_dir = os.path.join(td, 'spatial_cond_mom')
+        out_dir = os.path.join(out_dir_root, 'spatial_cond_mom')
         model.save(out_dir)
         loaded = model.load(out_dir)
 
@@ -116,6 +124,12 @@ def test_train_spatial(log=False, full_shape=(20, 20),
 
 
 if __name__ == "__main__":
-    test_train_st(n_epoch=4, log=True, full_shape=(20, 20))
+    # test_train_st(n_epoch=4, log=True, full_shape=(20, 20),
+    #               batch_size=4, n_batches=20,
+    #               out_dir_root='st_model')
+    test_train_spatial(n_epoch=4, log=True, full_shape=(20, 20),
+                       sample_shape=(10, 10, 1),
+                       batch_size=12, n_batches=40,
+                       out_dir_root='s_model')
     # test_train_spatial(log=True, full_shape=(20, 20),
     #                    sample_shape=(10, 10, 1), n_epoch=4)
