280ecb4d6f1aabbad9449028e9c5c7176751f710
Malik <malik.hassanaly@gmail.com>
2022-11-14 13:11:20 -0700

    add loss and timing plotting

--- tests/test_out_conditional_moments.py
+++ tests/test_out_conditional_moments.py
@@ -3,6 +3,7 @@
 import os
 # import json
 import numpy as np
+from pandas import read_csv
 
 from sup3r import TEST_DATA_DIR
 from sup3r import CONFIG_DIR
@@ -178,15 +179,87 @@ def test_out_spatial_mom2(plot=False, full_shape=(20, 20),
                   fps=6)
 
 
+def test_out_loss(plot=False, model_dirs=None,
+                  history_files=None, model_names=None):
+    """Loss convergence plotting of multiple models"""
+    # Load history
+    if model_dirs is not None:
+        history_files = [os.path.join(path, 'history.csv')
+                         for path in model_dirs]
+    elif history_files is not None:
+        pass
+    else:
+        print("No history file provided")
+        return
+
+    # Get model names
+    if model_names is None:
+        model_names = ["model_" + str(i)
+                       for i in range(len(history_files))]
+
+    # Read csv
+    histories = [read_csv(file) for file in history_files]
+    if plot:
+        import matplotlib.pyplot as plt
+        import matplotlib.pylab as pl
+        from sup3r.utilities.plot_utilities import (prettyLabels,
+                                                    plotLegend)
+        figureFolder = 'Figures'
+        os.makedirs(figureFolder, exist_ok=True)
+        figureLossFolder = os.path.join(figureFolder, 'loss')
+        os.makedirs(figureLossFolder, exist_ok=True)
+
+        epoch_id = histories[0].columns.get_loc('epoch')
+        time_id = histories[0].columns.get_loc('elapsed_time')
+        train_loss_id = histories[0].columns.get_loc('train_loss_gen')
+        test_loss_id = histories[0].columns.get_loc('val_loss_gen')
+        datas = [history.values for history in histories]
+
+        colors = pl.cm.jet(np.linspace(0, 1, len(histories)))
+
+        _ = plt.figure()
+        for idata, data in enumerate(datas):
+            plt.plot(data[:, epoch_id], np.diff(data[:, time_id],
+                     prepend=0),
+                     color=colors[idata], linewidth=3,
+                     label=model_names[idata])
+        prettyLabels('Epoch', 'Wall clock [s]', 14)
+        plt.savefig(os.path.join(figureLossFolder, 'timing.png'))
+        plt.close()
+
+        _ = plt.figure()
+        # test train labels
+        plt.plot(datas[0][:, epoch_id], datas[0][:, train_loss_id],
+                 color='k', linewidth=3, label='train')
+        plt.plot(datas[0][:, epoch_id], datas[0][:, test_loss_id],
+                 '--', color='k', linewidth=3, label='test')
+        # model labels
+        for idata, data in enumerate(datas):
+            plt.plot(data[:, epoch_id], data[:, train_loss_id],
+                     color=colors[idata], linewidth=3,
+                     label=model_names[idata])
+            plt.plot(data[:, epoch_id], data[:, test_loss_id],
+                     '--', color=colors[idata], linewidth=3)
+        prettyLabels('Epoch', 'Loss', 14)
+        plotLegend()
+        plt.savefig(os.path.join(figureLossFolder, 'loss_lin.png'))
+        ax = plt.gca()
+        ax.set_yscale('log')
+        plt.savefig(os.path.join(figureLossFolder, 'loss_log.png'))
+        plt.close()
+
+
 if __name__ == "__main__":
 
-    test_out_spatial_mom1(plot=True, full_shape=(20, 20),
-                          sample_shape=(10, 10, 1),
-                          batch_size=4, n_batches=4,
-                          s_enhance=2, model_dir='s_mom1/spatial_cond_mom')
+    test_out_loss(plot=True, model_dirs=['s_mom1/spatial_cond_mom'])
 
-    test_out_spatial_mom2(plot=True, full_shape=(20, 20),
-                          sample_shape=(10, 10, 1),
-                          batch_size=4, n_batches=4,
-                          s_enhance=2, model_dir='s_mom2/spatial_cond_mom',
-                          model_mom1_dir='s_mom1/spatial_cond_mom')
+    # test_out_spatial_mom1(plot=True, full_shape=(20, 20),
+    #                       sample_shape=(10, 10, 1),
+    #                       batch_size=4, n_batches=4,
+    #                       s_enhance=2, model_dir='s_mom1/spatial_cond_mom')
+
+    # test_out_spatial_mom2(plot=True, full_shape=(20, 20),
+    #                       sample_shape=(10, 10, 1),
+    #                       batch_size=4, n_batches=4,
+    #                       s_enhance=2, model_dir='s_mom2/spatial_cond_mom',
+    #                       model_mom1_dir='s_mom1/spatial_cond_mom')
