bug fix on windgan implementation of gradient descent with topo exo feature

sup3r/models/wind.py
@@ -163,10 +163,11 @@ class WindGan(Sup3rGan, AbstractWindInterface):
 
         return super().calc_loss(hi_res_true, hi_res_gen, **kwargs)
 
-    def run_gradient_descent(self, low_res, hi_res_true, training_weights,
-                             optimizer=None, **calc_loss_kwargs):
-        """Run gradient descent for one mini-batch of (low_res, hi_res_true)
-        and adjust NN weights
+    @tf.function()
+    def get_single_grad(self, low_res, hi_res_true, training_weights,
+                        device_name=None, **calc_loss_kwargs):
+        """Run gradient descent for one mini-batch of (low_res, hi_res_true),
+        do not update weights, just return gradient details.
 
         Parameters
         ----------
@@ -181,37 +182,37 @@ class WindGan(Sup3rGan, AbstractWindInterface):
         training_weights : list
             A list of layer weights that are to-be-trained based on the
             current loss weight values.
-        optimizer : tf.keras.optimizers.Optimizer
-            Optimizer class to use to update weights. This can be different if
-            you're training just the generator or one of the discriminator
-            models. Defaults to the generator optimizer.
+        device_name : None | str
+            Optional tensorflow device name for GPU placement. Note that if a
+            GPU is available, variables will be placed on that GPU even if
+            device_name=None.
         calc_loss_kwargs : dict
             Kwargs to pass to the self.calc_loss() method
 
         Returns
         -------
+        grad : list
+            a list or nested structure of Tensors (or IndexedSlices, or None,
+            or CompositeTensor) representing the gradients for the
+            training_weights
         loss_details : dict
             Namespace of the breakdown of loss components
         """
 
         hi_res_topo = hi_res_true[..., -1:]
 
-        with tf.GradientTape(watch_accessed_variables=False) as tape:
-            tape.watch(training_weights)
+        with tf.device(device_name):
+            with tf.GradientTape(watch_accessed_variables=False) as tape:
+                tape.watch(training_weights)
 
-            hi_res_gen = self._tf_generate(low_res, hi_res_topo)
-            loss_out = self.calc_loss(hi_res_true, hi_res_gen,
-                                      **calc_loss_kwargs)
-            loss, loss_details = loss_out
+                hi_res_gen = self._tf_generate(low_res, hi_res_topo)
+                loss_out = self.calc_loss(hi_res_true, hi_res_gen,
+                                          **calc_loss_kwargs)
+                loss, loss_details = loss_out
 
-            grad = tape.gradient(loss, training_weights)
+                grad = tape.gradient(loss, training_weights)
 
-        if optimizer is None:
-            optimizer = self.optimizer
-
-        optimizer.apply_gradients(zip(grad, training_weights))
-
-        return loss_details
+        return grad, loss_details
 
     def calc_val_loss(self, batch_handler, weight_gen_advers, loss_details):
         """Calculate the validation loss at the current state of model training