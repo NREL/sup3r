5fb65694dc5d925af31c0bfb08450fc428702447
grantbuster <grant.buster@nrel.gov>
2023-03-31 13:41:06 -0600

    added test for exo data cache

---
+++ tests/test_exo_data_handling.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""pytests for exogenous data handling"""
+import shutil
+import os
+import numpy as np
+
+from sup3r import TEST_DATA_DIR
+from sup3r.preprocessing.exogenous_data_handling import ExogenousDataHandler
+
+
+FP_WTK = os.path.join(TEST_DATA_DIR, 'test_wtk_co_2012.h5')
+
+FILE_PATHS = [os.path.join(TEST_DATA_DIR, 'ua_test.nc'),
+              os.path.join(TEST_DATA_DIR, 'va_test.nc'),
+              os.path.join(TEST_DATA_DIR, 'orog_test.nc'),
+              os.path.join(TEST_DATA_DIR, 'zg_test.nc')]
+FEATURES = ['topography']
+TARGET = (13.67, 125.0)
+SHAPE = (8, 8)
+S_ENHANCE = [1, 4]
+AGG_FACTORS = [4, 1]
+
+
+def test_exo_cache():
+    """Test exogenous data caching and re-load"""
+    # no cached data
+    try:
+        base = ExogenousDataHandler(FILE_PATHS, FEATURES, FP_WTK, S_ENHANCE,
+                                    AGG_FACTORS, target=TARGET, shape=SHAPE,
+                                    input_handler='DataHandlerNCforCC')
+        for i, arr in enumerate(base.data):
+            assert arr.shape[0] == SHAPE[0] * S_ENHANCE[i]
+            assert arr.shape[1] == SHAPE[1] * S_ENHANCE[i]
+    except Exception as e:
+        if os.path.exists('./exo_cache/'):
+            shutil.rmtree('./exo_cache/')
+        raise e
+    else:
+        assert os.path.exists('./exo_cache/')
+        assert len(os.listdir('./exo_cache')) == 2
+
+    # load cached data
+    try:
+        cache = ExogenousDataHandler(FILE_PATHS, FEATURES, FP_WTK, S_ENHANCE,
+                                     AGG_FACTORS, target=TARGET, shape=SHAPE,
+                                     input_handler='DataHandlerNCforCC')
+    except Exception as e:
+        if os.path.exists('./exo_cache/'):
+            shutil.rmtree('./exo_cache/')
+        raise e
+    else:
+        assert os.path.exists('./exo_cache/')
+        assert len(os.listdir('./exo_cache')) == 2
+        for arr1, arr2 in zip(base.data, cache.data):
+            assert np.allclose(arr1, arr2)
+        shutil.rmtree('./exo_cache/')
