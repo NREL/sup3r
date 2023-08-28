9d9bbaf7e37aaf7b27ff07392e1cd02a6afc4314
ppinchuk <ppinchuk@nrel.gov>
2023-08-28 17:27:19 -0600

    Pipeline now uses gaps classes

--- sup3r/pipeline/pipeline_cli.py
+++ sup3r/pipeline/pipeline_cli.py
@@ -6,12 +6,8 @@ Pipeline CLI entry points.
 import click
 import logging
 
-from sup3r.pipeline.config import Sup3rPipelineConfig as PipelineConfig
-from sup3r.pipeline.pipeline import Sup3rPipeline as Pipeline
+from gaps.cli.pipeline import pipeline
 from sup3r import __version__
-
-from rex.utilities.utilities import get_class_properties
-from rex.utilities.execution import SubprocessManager
 
 
 logger = logging.getLogger(__name__)
@@ -26,14 +22,6 @@ def main(ctx, verbose):
     """Sup3r Pipeline Command Line Interface"""
     ctx.ensure_object(dict)
     ctx.obj['VERBOSE'] = verbose
-
-
-@main.command()
-def valid_config_keys():
-    """
-    Echo the valid Pipeline config keys
-    """
-    click.echo(', '.join(get_class_properties(PipelineConfig)))
 
 
 @main.command()
@@ -53,36 +41,11 @@ def valid_config_keys():
 @click.option('-v', '--verbose', is_flag=True,
               help='Flag to turn on debug logging. Default is not verbose.')
 @click.pass_context
-def from_config(ctx, config_file, cancel, monitor, background, verbose):
+def from_config(ctx, config_file, cancel, monitor, background, verbose=False):
     """Run sup3r pipeline from a config file."""
     ctx.ensure_object(dict)
-    verbose = any([verbose, ctx.obj.get('VERBOSE', False)])
-
-    if cancel:
-        Pipeline.cancel_all(config_file)
-    elif monitor and background:
-        pipeline_monitor_background(config_file, verbose=verbose)
-    else:
-        Pipeline.run(config_file, monitor=monitor, verbose=verbose)
-
-
-def pipeline_monitor_background(config_file, verbose=False):
-    """Submit the pipeline execution with monitoring in the background using
-    the nohup linux command.
-
-    Parameters
-    ----------
-    config_file : STR
-        Filepath to reV pipeline configuration json file.
-    verbose : bool
-        Flag to turn on debug logging for the pipeline.
-    """
-
-    cmd = (f'python -m sup3r.cli -c {config_file} pipeline --monitor')
-    if verbose:
-        cmd += ' -v'
-
-    SubprocessManager.submit(cmd, background=True, background_stdout=False)
+    ctx.obj['VERBOSE'] = verbose or ctx.obj.get('VERBOSE', False)
+    pipeline(config_file, cancel, monitor, background)
 
 
 if __name__ == '__main__':
