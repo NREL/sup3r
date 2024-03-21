7b2faf19ed58bc1fc30b7170175e324b3609afcf
Gui Castelao <guilherme@castelao.net>
2024-03-20 20:14:38 -0600

    feat: QDM.get_base_data()
    Simplifies get_base_data().

--- sup3r/bias/qdm.py
+++ sup3r/bias/qdm.py
@@ -9,5 +9,30 @@ logger = logging.getLogger(__name__)
 
 
 class QuantileDeltaMapping(DataRetrievalBase):
+    def get_base_data(self, base_gid, daily_reduction):
+        """Get base data for given GID
+
+        For now, there is no need to change most of the arguments, thus
+        this wrapper simplifies the DataRetrievalBase.get_base_data()
+        by using its context.
+
+        Parameters
+        ----------
+
+        Return
+        ------
+
+        """
+        base_data, _ = super().get_base_data(self.base_fps,
+                                             self.base_dset,
+                                             base_gid,
+                                             self.base_handler,
+                                             base_handler_kwargs=None,
+                                             daily_reduction=daily_reduction,
+                                             decimals=self.decimals,
+                                             base_dh_inst=self.base_dh
+                                             )
+        return base_data
+
     def run(self):
         logger.debug('Starting linear correction calculation...')
