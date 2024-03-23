8b976052ba6745277ad4f9fc3e802ec38a826867
Gui Castelao <guilherme@castelao.net>
2024-03-22 19:38:44 -0600

    feat: Custom QuantileDeltaMapping.__init__ to deal with biased future
    The QuantileDeltaMapping() requires a third dataset, the biased future,
    thus requiring a modified instantiation to receive such dataset.

--- sup3r/bias/qdm.py
+++ sup3r/bias/qdm.py
@@ -9,6 +9,24 @@ logger = logging.getLogger(__name__)
 
 
 class QuantileDeltaMapping(DataRetrievalBase):
+    def __init__(self, base_fps, bias_fps, bias_fut_fps, *args, **kwargs):
+        super().__init__(base_fps, bias_fps, *args, **kwargs)
+
+        self.bias_fut_fps = bias_fps
+
+        if isinstance(self.bias_fut_fps, str):
+            self.bias_fut_fps = sorted(glob(self.bias_fut_fps))
+
+        self.bias_fut_dh = self.bias_handler(self.bias_fut_fps,
+                                             [self.bias_feature],
+                                             target=self.target,
+                                             shape=self.shape,
+                                             val_split=0.0,
+                                             **self.bias_handler_kwargs)
+
+        assert np.allclose(self.bias_dh.lat_lon, self.bias_fut_dh.lat_lon)
+
+
     def get_base_data(self, base_gid, daily_reduction):
         """Get base data for given GID
 
