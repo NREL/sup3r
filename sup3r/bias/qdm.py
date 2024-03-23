20223af59d058677d70228f5fad6ed71c8d42499
Gui Castelao <guilherme@castelao.net>
2024-03-22 19:44:24 -0600

    feat: QuantileDeltaMapping.run() to estimate distributions
    An MVP of empirical distributions estimate for historical observations,
    historical modeled, and future modeled. Runing serial only.

--- sup3r/bias/qdm.py
+++ sup3r/bias/qdm.py
@@ -56,5 +56,41 @@ class QuantileDeltaMapping(DataRetrievalBase):
                                              )
         return base_data
 
-    def run(self):
-        logger.debug('Starting linear correction calculation...')
+    def run(self, daily_reduction='avg'):
+        """
+
+        Reminders:
+        - Options between relative or absolute correction
+        """
+        logger.debug("Starting linear correction calculation...")
+
+        logger.debug('Running serial calculation.')
+        self.NT = 51
+        keys = ["base_CDF", "bias_CDF", "bias_fut_CDF"]
+        self.out = {k: np.full((*self.bias_gid_raster.shape, self.NT),
+                               np.nan, np.float32)
+                    for k in keys}
+
+        for i, bias_gid in enumerate(self.bias_meta.index):
+            raster_loc = np.where(self.bias_gid_raster == bias_gid)
+            _, base_gid = self.get_base_gid(bias_gid)
+
+            if not base_gid.any():
+                self.bad_bias_gids.append(bias_gid)
+                logger.debug(f"No base data for bias_gid: {bias_gid}. "
+                             "Adding it to bad_bias_gids")
+            else:
+                bias_data = self.get_bias_data(bias_gid)
+                bias_fut_data = self.get_bias_data(bias_gid, self.bias_fut_dh)
+                base_data = self.get_base_data(base_gid, daily_reduction)
+
+                D_base = EmpiricalDistribution.from_fit(base_data, self.NT)
+                self.out['base_CDF'][raster_loc] = D_base.cut_point
+
+                D_bias = EmpiricalDistribution.from_fit(bias_data, self.NT)
+                self.out['bias_CDF'][raster_loc] = D_bias.cut_point
+                D_bias_fut = EmpiricalDistribution.from_fit(bias_fut_data, self.NT)
+                self.out['bias_fut_CDF'][raster_loc] = D_bias_fut.cut_point
+
+            logger.info('Completed bias calculations for {} out of {} '
+                            'sites'.format(i + 1, len(self.bias_meta)))
