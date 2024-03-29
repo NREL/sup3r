278347fe9d3fcaa22e7f031ed4b1d47b2a0eb710
Gui Castelao <guilherme@castelao.net>
2024-03-29 13:42:11 -0600

    style: Arguments alignment

--- tests/bias/test_qdm_bias_correction.py
+++ tests/bias/test_qdm_bias_correction.py
@@ -29,10 +29,10 @@ def test_qdm_bc():
     """Test QDM bias correction"""
 
     calc = QuantileDeltaMappingCorrection(FP_NSRDB, FP_CC, FP_FUT_CC,
-                            'ghi', 'rsds',
-                            target=TARGET, shape=SHAPE,
-                            distance_upper_bound=0.7,
-                            bias_handler='DataHandlerNCforCC')
+                                          'ghi', 'rsds',
+                                          target=TARGET, shape=SHAPE,
+                                          distance_upper_bound=0.7,
+                                          bias_handler='DataHandlerNCforCC')
 
     out = calc.run()
 
@@ -46,17 +46,17 @@ def test_parallel():
     """Compare bias correction run serial vs in parallel"""
 
     s = QuantileDeltaMappingCorrection(FP_NSRDB, FP_CC, FP_FUT_CC,
-                            'ghi', 'rsds',
-                            target=TARGET, shape=SHAPE,
-                            distance_upper_bound=0.7,
-                            bias_handler='DataHandlerNCforCC')
+                                       'ghi', 'rsds',
+                                       target=TARGET, shape=SHAPE,
+                                       distance_upper_bound=0.7,
+                                       bias_handler='DataHandlerNCforCC')
     out_s = s.run(max_workers=1)
 
     p = QuantileDeltaMappingCorrection(FP_NSRDB, FP_CC, FP_FUT_CC,
-                            'ghi', 'rsds',
-                            target=TARGET, shape=SHAPE,
-                            distance_upper_bound=0.7,
-                            bias_handler='DataHandlerNCforCC')
+                                       'ghi', 'rsds',
+                                       target=TARGET, shape=SHAPE,
+                                       distance_upper_bound=0.7,
+                                       bias_handler='DataHandlerNCforCC')
     out_p = p.run(max_workers=2)
 
     for k in out_s.keys():
