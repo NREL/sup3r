b521138869cd32ba3aff50e0437c04c3e8b8648b
Gui Castelao <guilherme@castelao.net>
2024-03-29 11:10:03 -0600

    test: basic run of QuantileDeltaMappingCorrection

--- tests/bias/test_bias_qdm_correction.py
+++ tests/bias/test_bias_qdm_correction.py
@@ -1,3 +1,23 @@
+"""pytests QDM bias correction calculations"""
+
+
+def test_qdm_bc():
+    """Test QDM bias correction"""
+
+    calc = QuantileDeltaMappingCorrection(FP_NSRDB, FP_CC, FP_FUT_CC,
+                            'ghi', 'rsds',
+                            target=TARGET, shape=SHAPE,
+                            distance_upper_bound=0.7,
+                            bias_handler='DataHandlerNCforCC')
+
+    out = calc.run()
+
+    # Guarantee that we have some actual values, otherwise most of the
+    # remaining tests would be useless
+    for v in out:
+        assert np.isfinite(out[v]).any()
+
+
 def test_parallel():
     """Compare bias correction run serial vs in parallel"""
 
