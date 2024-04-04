71bb5f6ac2e5ddd1f7264a1ab576f49de17f28f3
Gui Castelao <guilherme@castelao.net>
2024-04-03 18:41:12 -0600

    test: DataHandler.qdm_bc()

--- tests/bias/test_qdm_bias_correction.py
+++ tests/bias/test_qdm_bias_correction.py
@@ -10,6 +10,8 @@ import xarray as xr
 
 from sup3r import TEST_DATA_DIR
 from sup3r.bias.bias_calc import QuantileDeltaMappingCorrection
+from sup3r.bias.bias_transforms import local_qdm_bc
+from sup3r.preprocessing.data_handling import DataHandlerNC
 
 FP_NSRDB = os.path.join(TEST_DATA_DIR, "test_nsrdb_co_2018.h5")
 FP_CC = os.path.join(TEST_DATA_DIR, "rsds_test.nc")
@@ -87,3 +89,35 @@ def test_save_file():
         # A valid HDF5, can open and read
         with h5py.File(filename, "r") as f:
             assert "latitude" in f.keys()
+
+
+def test_qdm_transform():
+    calc = QuantileDeltaMappingCorrection(FP_NSRDB, FP_CC, FP_FUT_CC,
+                                          'ghi', 'rsds',
+                                          target=TARGET, shape=SHAPE,
+                                          distance_upper_bound=0.7,
+                                          bias_handler='DataHandlerNCforCC')
+    lat_lon = calc.bias_dh.lat_lon
+    with tempfile.TemporaryDirectory() as td:
+        fp_out = os.path.join(td, 'bc.h5')
+        _ = calc.run(max_workers=1, fp_out=fp_out)
+        data = np.ones((*lat_lon.shape[:-1], 2))
+        corrected = local_qdm_bc(data, lat_lon, 'ghi', 'rsds', fp_out)
+
+
+def test_handler_qdm_bc():
+    """qdm_bc() method from DataHandler
+
+    WIP: Confirm it runs, but don't verify anything yet.
+    """
+    calc = QuantileDeltaMappingCorrection(FP_NSRDB, FP_CC, FP_FUT_CC,
+                                          'ghi', 'rsds',
+                                          target=TARGET, shape=SHAPE,
+                                          distance_upper_bound=0.7,
+                                          bias_handler='DataHandlerNCforCC')
+    with tempfile.TemporaryDirectory() as td:
+        fp_out = os.path.join(td, 'bc.h5')
+        _ = calc.run(max_workers=1, fp_out=fp_out)
+
+        Handler = DataHandlerNC(FP_FUT_CC, 'rsds')
+        Handler.qdm_bc(fp_out, 'ghi')
