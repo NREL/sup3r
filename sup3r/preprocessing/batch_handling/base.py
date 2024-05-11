87151684a8308d0e03f0d34fc78de15a22a1843f
bnb32 <brandon.benton@nrel.gov>
2024-05-10 18:57:13 -0600

    new structure for batch interfacing objects working with lazy loaders.

--- sup3r/preprocessing/batch_handling/base.py
+++ sup3r/preprocessing/batch_handling/base.py
@@ -38,7 +38,7 @@ class ValidationData(AbstractBatchBuilder):
     """Iterator for validation data"""
 
     # Classes to use for handling an individual batch obj.
-    BATCH_CLASS = Batch
+    BATCH_CLASS = SingleBatch
 
     def __init__(self,
                  data_handlers,
@@ -224,7 +224,7 @@ class BatchHandler(MultiHandlerMixIn, AbstractBatchBuilder):
 
     # Classes to use for handling an individual batch obj.
     VAL_CLASS = ValidationData
-    BATCH_CLASS = Batch
+    BATCH_CLASS = SingleBatch
     DATA_HANDLER_CLASS = None
 
     def __init__(self,
