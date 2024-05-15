fe591a459e572df60c945d64cef52d14a59379b1
Brandon N. Benton <brandon.benton@nrel.gov>
2024-05-15 10:12:08 -0600

    fixed _log_args arg parsing.

--- sup3r/containers/abstract.py
+++ sup3r/containers/abstract.py
@@ -46,9 +46,9 @@ class AbstractContainer(ABC, metaclass=_ContainerMeta):
         """Log argument names and values."""
         arg_spec = inspect.getfullargspec(cls.__init__)
         args = args or []
+        defaults = arg_spec.defaults or []
         arg_names = arg_spec.args[1:]  # exclude self
         args_dict = dict(zip(arg_names[:len(args)], args))
-        defaults = arg_spec.defaults or []
         default_dict = dict(zip(arg_names[-len(defaults):], defaults))
         args_dict.update(default_dict)
         args_dict.update(kwargs)
