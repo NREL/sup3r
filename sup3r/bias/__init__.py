89b4bab8eb23ee0f85ffd084afd032ad78451ffa
Gui Castelao <guilherme@castelao.net>
2024-05-20 10:22:02 -0600

    Defining `__all__` for bias
    Defining what to expose with wildcard.

--- sup3r/bias/__init__.py
+++ sup3r/bias/__init__.py
@@ -1,7 +1,18 @@
 # -*- coding: utf-8 -*-
 """Bias calculation and correction modules."""
+
 from .bias_transforms import (global_linear_bc, local_linear_bc,
                               monthly_local_linear_bc)
 from .bias_calc import (LinearCorrection, MonthlyLinearCorrection,
                         SkillAssessment)
 from .qdm import QuantileDeltaMappingCorrection
+
+__all__ = [
+    "global_linear_bc",
+    "local_linear_bc",
+    "monthly_local_linear_bc",
+    "LinearCorrection",
+    "MonthlyLinearCorrection",
+    "QuantileDeltaMappingCorrection",
+    "SkillAssessment",
+]
