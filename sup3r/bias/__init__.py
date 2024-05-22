c69951c60fe614780c265b6643ffde41b8171aa3
Gui Castelao <guilherme@castelao.net>
2024-05-22 11:38:14 -0600

    refact: local_qdm_bc available as a bias resource
    Following the pattern, `local_qdm_bc` should be available as a bias
    resource as well. I missed that when I was implementing it.

--- sup3r/bias/__init__.py
+++ sup3r/bias/__init__.py
@@ -1,7 +1,7 @@
 """Bias calculation and correction modules."""
 
 from .bias_transforms import (global_linear_bc, local_linear_bc,
-                              monthly_local_linear_bc)
+                              local_qdm_bc, monthly_local_linear_bc)
 from .bias_calc import (LinearCorrection, MonthlyLinearCorrection,
                         SkillAssessment)
 from .qdm import QuantileDeltaMappingCorrection
@@ -9,6 +9,7 @@ from .qdm import QuantileDeltaMappingCorrection
 __all__ = [
     "global_linear_bc",
     "local_linear_bc",
+    "local_qdm_bc",
     "monthly_local_linear_bc",
     "LinearCorrection",
     "MonthlyLinearCorrection",
