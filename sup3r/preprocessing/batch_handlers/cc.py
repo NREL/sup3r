cc samplers smoke tests passing. working out normalization and nan issues. some checks on correct inputs types for cc batcher/sampler added.

sup3r/preprocessing/batch_handlers/cc.py
@@ -1,7 +1,4 @@
-"""
-Sup3r batch_handling module.
-@author: bbenton
-"""
+"""Batch Handler for hourly -> daily climate data downscaling."""
 
 import logging
 
@@ -29,34 +26,70 @@ class BatchHandlerCC(BaseHandlerCC):
     """Batch handling class for climate change data with daily averages as the
     coarse dataset."""
 
-    def __init__(self, *args, sub_daily_shape=None, **kwargs):
+    def __init__(
+        self, *args, sub_daily_shape=None, coarsen_kwargs=None, **kwargs
+    ):
         """
         Parameters
         ----------
         *args : list
-            Same positional args as BatchHandler
+            Same positional args as parent class
         sub_daily_shape : int
             Number of hours to use in the high res sample output. This is the
             shape of the temporal dimension of the high res batch observation.
             This time window will be sampled for the daylight hours on the
             middle day of the data handler observation.
+        coarsen_kwargs : Union[Dict, None]
+            Dictionary of kwargs to be passed to `self.coarsen`.
         **kwargs : dict
-            Same keyword args as BatchHandler
+            Same keyword args as parent class
         """
         t_enhance = kwargs.get('t_enhance', 24)
-        msg = (f'{self.__class__.__name__} does not yet support t_enhance '
-               f'!= 24. Received t_enhance = {t_enhance}.')
+        msg = (
+            f'{self.__class__.__name__} does not yet support t_enhance '
+            f'!= 24. Received t_enhance = {t_enhance}.'
+        )
         assert t_enhance == 24, msg
         super().__init__(*args, **kwargs)
         self.sub_daily_shape = sub_daily_shape
+        self.coarsen_kwargs = coarsen_kwargs or {
+            'smoothing_ignore': [],
+            'smoothing': None,
+        }
 
-    def coarsen(self, samples):
-        """Subsample hourly data to the daylight window and coarsen the daily
-        data. Smooth if requested."""
-        low_res, high_res = samples
-        high_res = high_res[..., self.hr_features_ind]
-        high_res = self.reduce_high_res_sub_daily(high_res)
-        low_res = spatial_coarsening(low_res, self.s_enhance)
+    def batch_next(self, samples):
+        """Down samples and coarsens daily samples, normalizes low / high res
+        and returns wrapped collection of samples / observations."""
+        lr, hr = self.coarsen(samples, **self.coarsen_kwargs)
+        lr, hr = self.normalize(lr, hr)
+        return self.BATCH_CLASS(low_res=lr, high_res=hr)
+
+    def coarsen(
+        self,
+        samples,
+        smoothing=None,
+        smoothing_ignore=None,
+    ):
+        """Coarsen high res data to get corresponding low res batch. For this
+        special CC handler this means: subsample hourly data to the daylight
+        window and coarsen the daily data. Smooth if requested.
+
+        TODO: Remove call to `spatial_coarsening` and perform this before
+        queueing samples, so we can unify more with main `DualSampler` pattern.
+
+        Note
+        ----
+        `samples` here is a Tuple (daily, hourly), in contrast to `coarsen` in
+        `SingleBatchQueue.coarsen` which just takes `samples` = `high_res`
+
+        See Also
+        --------
+        :meth:`SingleBatchQueue.coarsen`
+        """
+        daily, hourly = samples
+        hourly = hourly.numpy()[..., self.hr_features_ind]
+        high_res = self.reduce_high_res_sub_daily(hourly)
+        low_res = spatial_coarsening(daily, self.s_enhance)
 
         if (
             self.hr_out_features is not None
@@ -66,16 +99,16 @@ class BatchHandlerCC(BaseHandlerCC):
             if np.isnan(high_res[..., i_cs]).any():
                 high_res[..., i_cs] = nn_fill_array(high_res[..., i_cs])
 
-        if self.smoothing is not None:
+        if smoothing is not None:
             feat_iter = [
                 j
                 for j in range(low_res.shape[-1])
-                if self.features[j] not in self.smoothing_ignore
+                if self.features[j] not in smoothing_ignore
             ]
             for i in range(low_res.shape[0]):
                 for j in feat_iter:
                     low_res[i, ..., j] = gaussian_filter(
-                        low_res[i, ..., j], self.smoothing, mode='nearest'
+                        low_res[i, ..., j], smoothing, mode='nearest'
                     )
         return low_res, high_res
 

sup3r/preprocessing/derivers/methods.py
@@ -71,9 +71,9 @@ class ClearSkyRatioH5(DerivedFeature):
         # set any timestep with any nighttime equal to NaN to avoid weird
         # sunrise/sunset artifacts.
         night_mask = night_mask.any(axis=(0, 1)).compute()
-        data['clearsky_ghi', ..., night_mask] = np.nan
 
         cs_ratio = data['ghi', ...] / data['clearsky_ghi', ...]
+        cs_ratio[..., night_mask] = np.nan
         return cs_ratio.astype(np.float32)
 
 

sup3r/preprocessing/samplers/base.py
@@ -7,9 +7,10 @@ from fnmatch import fnmatch
 from typing import Dict, Optional, Tuple
 from warnings import warn
 
+import numpy as np
 import xarray as xr
 
-from sup3r.preprocessing.base import Container
+from sup3r.preprocessing.base import Container, Sup3rDataset, Sup3rX
 from sup3r.preprocessing.common import lowered
 from sup3r.utilities.utilities import uniform_box_sampler, uniform_time_sampler
 
@@ -21,16 +22,16 @@ class Sampler(Container):
 
     def __init__(
         self,
-        data: xr.Dataset,
+        data: xr.Dataset | Sup3rX | Sup3rDataset,
         sample_shape,
         feature_sets: Optional[Dict] = None,
     ):
         """
         Parameters
         ----------
-        data : xr.Dataset
-            xr.Dataset() object with data that will be sampled from.  Can be
-            the `.data` attribute of various :class:`Container` objects.  i.e.
+        data : xr.Dataset | Sup3rX | Sup3rDataset
+            Object with data that will be sampled from. Can be the `.data`
+            attribute of various :class:`Container` objects.  i.e.
             :class:`Loader`, :class:`Extracter`, :class:`Deriver`, as long as
             the spatial dimensions are not flattened.
         sample_shape : tuple
@@ -41,8 +42,7 @@ class Sampler(Container):
 
             features : list | tuple
                 List of full set of features to use for sampling. If no entry
-                is provided then all features / data_vars from data will be
-                used.
+                is provided then all data_vars from data will be used.
             lr_only_features : list | tuple
                 List of feature names or patt*erns that should only be
                 included in the low-res training set and not the high-res
@@ -61,7 +61,6 @@ class Sampler(Container):
         self._counter = 0
         self.sample_shape = sample_shape
         self.lr_features = self.features
-        self.hr_features = self.features
         self.preflight()
 
     def get_sample_index(self):
@@ -224,3 +223,24 @@ class Sampler(Container):
             raise RuntimeError(msg)
 
         return lowered(out)
+
+    @property
+    def hr_features_ind(self):
+        """Get the high-resolution feature channel indices that should be
+        included for training. Any high-resolution features that are only
+        included in the data handler to be coarsened for the low-res input are
+        removed"""
+        hr_features = list(self.hr_out_features) + list(self.hr_exo_features)
+        if list(self.features) == hr_features:
+            return np.arange(len(self.features))
+        return [
+            i
+            for i, feature in enumerate(self.features)
+            if feature in hr_features
+        ]
+
+    @property
+    def hr_features(self):
+        """Get the high-resolution features corresponding to
+        `hr_features_ind`"""
+        return [self.features[ind].lower() for ind in self.hr_features_ind]

sup3r/preprocessing/samplers/cc.py
@@ -3,10 +3,9 @@
 """
 
 import logging
-from typing import Dict, Optional, Tuple
+from typing import Dict, Optional
 
 import numpy as np
-import xarray as xr
 
 from sup3r.preprocessing.base import Sup3rDataset
 from sup3r.preprocessing.samplers.base import Sampler
@@ -30,11 +29,19 @@ class DualSamplerCC(Sampler):
     version of the hourly data. It will ultimately be coarsened spatially
     before constructing batches. Here we are constructing a sampler to sample
     the daily / hourly pairs so we use an "lr_sample_shape" which is only
-    temporally low resolution."""
+    temporally low resolution.
+
+    TODO: With the dalyed computations from dask we could spatially coarsen
+    the daily data here and then use the standard `DualSampler` methods (most,
+    anyway, `get_sample_index` would need to be slightly different, I
+    think.). The call to `spatial_coarsening` in `coarsen` could then be
+    removed and only temporal down sampling for the hourly data would be
+    performed there.
+    """
 
     def __init__(
         self,
-        data: Sup3rDataset | Tuple[xr.Dataset, xr.Dataset],
+        data: Sup3rDataset,
         sample_shape=None,
         s_enhance=1,
         t_enhance=24,
@@ -43,12 +50,38 @@ class DualSamplerCC(Sampler):
         """
         Parameters
         ----------
-        data : Sup3rDataset | Tuple[xr.Dataset, xr.Dataset]
-            A tuple of xr.Dataset instances. The first must be daily
-            and the second must be hourly data
+        data : Sup3rDataset
+            A tuple of xr.Dataset instances wrapped in the
+            :class:`Sup3rDataset` interface. The first must be daily and the
+            second must be hourly data
+        sample_shape : tuple
+            Size of arrays to sample from the high-res data. The sample shape
+            for the low-res sampler will be determined from the enhancement
+            factors.
+        s_enhance : int
+            Spatial enhancement factor
+        t_enhance : int
+            Temporal enhancement factor
+        feature_sets : Optional[dict]
+            Optional dictionary describing how the full set of features is
+            split between `lr_only_features` and `hr_exo_features`.
+
+            lr_only_features : list | tuple
+                List of feature names or patt*erns that should only be
+                included in the low-res training set and not the high-res
+                observations.
+            hr_exo_features : list | tuple
+                List of feature names or patt*erns that should be included
+                in the high-resolution observation but not expected to be
+                output from the generative model. An example is high-res
+                topography that is to be injected mid-network.
         """
-        n_hours = data.high_res.sizes['time']
-        n_days = data.low_res.sizes['time']
+        msg = (f'{self.__class__.__name__} requires a Sup3rDataset object '
+               'with `.daily` and `.hourly` data members, in that order')
+        assert hasattr(data, 'daily') and hasattr(data, 'hourly'), msg
+        assert data.daily == data[0] and data.hourly == data[1], msg
+        n_hours = data.hourly.sizes['time']
+        n_days = data.daily.sizes['time']
         self.daily_data_slices = [
             slice(x[0], x[-1] + 1)
             for x in np.array_split(np.arange(n_hours), n_days)
@@ -131,12 +164,12 @@ class DualSamplerCC(Sampler):
         obs_ind_daily = (
             *spatial_slice,
             t_slice_daily,
-            self.data.low_res.features,
+            self.data.daily.features,
         )
         obs_ind_hourly = (
             *spatial_slice,
             t_slice_hourly,
-            self.data.high_res.features,
+            self.data.hourly.features,
         )
 
         return (obs_ind_daily, obs_ind_hourly)

tests/samplers/test_cc.py
@@ -14,7 +14,7 @@ from sup3r.preprocessing import (
     DataHandlerH5SolarCC,
     DualSamplerCC,
 )
-from sup3r.utilities.pytest.helpers import execute_pytest
+from sup3r.utilities.pytest.helpers import TestDualSamplerCC, execute_pytest
 from sup3r.utilities.utilities import nsrdb_sub_daily_sampler, pd_date_range
 
 SHAPE = (20, 20)
@@ -49,38 +49,53 @@ def test_solar_handler_sampling(plot=False):
     with NaN values for nighttime."""
 
     handler = DataHandlerH5SolarCC(
-            INPUT_FILE_S,
-            features=['clearsky_ratio'],
-            target=TARGET_S,
-            shape=SHAPE,
-        )
+        INPUT_FILE_S,
+        features=['clearsky_ratio'],
+        target=TARGET_S,
+        shape=SHAPE,
+    )
     assert ['clearsky_ghi', 'ghi'] not in handler
     assert 'clearsky_ratio' in handler
 
     handler = DataHandlerH5SolarCC(
-        INPUT_FILE_S, features=FEATURES_S, **dh_kwargs)
+        INPUT_FILE_S, features=FEATURES_S, **dh_kwargs
+    )
     assert ['clearsky_ghi', 'ghi', 'clearsky_ratio'] in handler
 
-    sampler = DualSamplerCC(handler, sample_shape)
+    sampler = TestDualSamplerCC(handler.data, sample_shape)
 
     assert handler.data.shape[2] % 24 == 0
     assert sampler.data.shape[2] % 24 == 0
 
     # some of the raw clearsky ghi and clearsky ratio data should be loaded in
     # the handler as NaN but the daily data should not have any NaN values
-    assert np.isnan(handler.data[...]).any()
-    assert np.isnan(sampler.data[...][1]).any()
-    assert not np.isnan(handler.daily_data.sx[...]).any()
-    assert not np.isnan(sampler.data[...][0]).any()
+    assert np.isnan(handler.data.hourly.as_array()).any()
+    assert np.isnan(sampler.data.hourly.as_array()).any()
+    assert not np.isnan(handler.data.daily.as_array()).any()
+    assert not np.isnan(sampler.data.daily.as_array()).any()
+
+    assert np.array_equal(
+        handler.data.daily.as_array(), sampler.data.daily.as_array()
+    )
+    assert np.allclose(
+        handler.data.hourly.as_array(),
+        sampler.data.hourly.as_array(),
+        equal_nan=True,
+    )
 
     for _ in range(10):
-        obs_ind_daily, obs_ind_hourly = sampler.get_sample_index()
-        assert obs_ind_hourly[2].start / 24 == obs_ind_daily[2].start
-        assert obs_ind_hourly[2].stop / 24 == obs_ind_daily[2].stop
-
         obs_daily, obs_hourly = sampler.get_next()
         assert obs_hourly.shape[2] == 24
         assert obs_daily.shape[2] == 1
+
+        obs_ind_daily, obs_ind_hourly = sampler.current_obs_index
+        assert obs_ind_hourly[2].start / 24 == obs_ind_daily[2].start
+        assert obs_ind_hourly[2].stop / 24 == obs_ind_daily[2].stop
+
+        assert np.array_equal(obs_daily, handler.data.daily[obs_ind_daily])
+        assert np.allclose(
+            obs_hourly, handler.data.hourly[obs_ind_hourly], equal_nan=True
+        )
 
         cs_ratio_profile = obs_hourly[0, 0, :, 0]
         assert np.isnan(cs_ratio_profile[0]) & np.isnan(cs_ratio_profile[-1])