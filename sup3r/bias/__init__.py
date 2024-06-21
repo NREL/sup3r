edd30058248959f81319fce33083ea39fbefcba9
Gui Castelao <guilherme@castelao.net>
2024-06-21 08:00:37 -0600

    Entry point for presrat functions

--- sup3r/bias/__init__.py
+++ sup3r/bias/__init__.py
@@ -1,15 +1,19 @@
 """Bias calculation and correction modules."""
 
-from .bias_transforms import (global_linear_bc, local_linear_bc,
-                              local_qdm_bc, monthly_local_linear_bc)
 from .bias_calc import (LinearCorrection, MonthlyLinearCorrection,
                         MonthlyScalarCorrection, SkillAssessment)
 from .qdm import PresRat, QuantileDeltaMappingCorrection
+from .bias_transforms import (apply_zero_precipitation_rate,
+                              global_linear_bc, local_linear_bc,
+                              local_qdm_bc, local_presrat_bc,
+                              monthly_local_linear_bc)
 
 __all__ = [
+    "apply_zero_precipitation_rate",
     "global_linear_bc",
     "local_linear_bc",
     "local_qdm_bc",
+    "local_presrat_bc",
     "monthly_local_linear_bc",
     "LinearCorrection",
     "MonthlyLinearCorrection",
