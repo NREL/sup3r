9ac27c1b74400af1da62e85785d27b91a8a73e2e
grantbuster <grantbuster@users.noreply.github.com>
2024-07-01 20:21:02 +0000

    Merge pull request #221 from NREL/gb/save_def_device
    save default device to model parameters so model weights can be loade…

---
+++ _static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.1.3.dev147+gee6ab7b4',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
