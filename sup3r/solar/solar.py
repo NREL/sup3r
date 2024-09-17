8ae857d883c15bd218c5f516e9e534fdb928c1ac
Brandon N. Benton <brandon.benton@nrel.gov>
2024-09-17 15:43:22 -0600

    dont need time shift in the solar module. can just shift this in a separate script if this is an issue.

--- sup3r/solar/solar.py
+++ sup3r/solar/solar.py
@@ -35,7 +35,6 @@ class Solar:
         nsrdb_fp,
         t_slice=slice(None),
         tz=-6,
-        time_shift=0,
         agg_factor=1,
         nn_threshold=0.5,
         cloud_threshold=0.99,
@@ -67,12 +66,6 @@ class Solar:
             the GAN is trained on data in local time and therefore the output
             in sup3r_fps should be treated as local time. For example, -6 is
             CST which is default for CONUS training data.
-        time_shift : int
-            Number of hours to shift time axis. This can be used, for
-            example, to shift the time index for daily data so that the time
-            stamp for a given day starts at hour zero instead of at
-            noon, as is the case for most GCM data. In this case ``time_shift``
-            would be -12
         agg_factor : int
             Spatial aggregation factor for nsrdb-to-GAN-meta e.g. the number of
             NSRDB spatial pixels to average for a single sup3r GAN output site.
@@ -91,7 +84,6 @@ class Solar:
         self.nn_threshold = nn_threshold
         self.cloud_threshold = cloud_threshold
         self.tz = tz
-        self.time_shift = time_shift
         self._nsrdb_fp = nsrdb_fp
         self._sup3r_fps = sup3r_fps
         if isinstance(self._sup3r_fps, str):
@@ -203,8 +195,7 @@ class Solar:
         -------
         pd.DatetimeIndex
         """
-        ti = self.gan_data.time_index[self.t_slice]
-        return ti.shift(self.time_shift, freq='h')
+        return self.gan_data.time_index[self.t_slice]
 
     @property
     def out_of_bounds(self):
@@ -596,7 +587,6 @@ class Solar:
         nsrdb_fp,
         fp_out_suffix='irradiance',
         tz=-6,
-        time_shift=0,
         agg_factor=1,
         nn_threshold=0.5,
         cloud_threshold=0.99,
@@ -623,12 +613,6 @@ class Solar:
             the GAN is trained on data in local time and therefore the output
             in sup3r_fps should be treated as local time. For example, -6 is
             CST which is default for CONUS training data.
-        time_shift : int | None
-            Number of hours to shift time axis. This can be used, for
-            example, to shift the time index for daily data so that the time
-            stamp for a given day starts at hour zero instead of at
-            noon, as is the case for most GCM data. In this case ``time_shift``
-            would be -12
         agg_factor : int
             Spatial aggregation factor for nsrdb-to-GAN-meta e.g. the number of
             NSRDB spatial pixels to average for a single sup3r GAN output site.
@@ -680,7 +664,6 @@ class Solar:
             kwargs = {
                 't_slice': t_slice,
                 'tz': tz,
-                'time_shift': time_shift,
                 'agg_factor': agg_factor,
                 'nn_threshold': nn_threshold,
                 'cloud_threshold': cloud_threshold,
