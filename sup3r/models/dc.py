ba46c5e70d986e412f2d0553bad402d065aa668a
bnb32 <brandon.benton@nrel.gov>
2025-05-01 06:44:45 -0600

    remove redundant calcs in Sup3rGanDC val loss

--- sup3r/models/dc.py
+++ sup3r/models/dc.py
@@ -33,60 +33,34 @@ class Sup3rGanDC(Sup3rGan):
 
         Returns
         -------
-        array
+        total_losses : np.ndarray
             Array of total losses for all sample bins, with shape
             (n_space_bins, n_time_bins)
+        content_losses : np.ndarray
+            Array of content losses for all sample bins, with shape
+            (n_space_bins, n_time_bins)
         """
-        losses = np.zeros(
+        total_losses = np.zeros(
+            (batch_handler.n_space_bins, batch_handler.n_time_bins),
+            dtype=np.float32,
+        )
+        content_losses = np.zeros(
             (batch_handler.n_space_bins, batch_handler.n_time_bins),
             dtype=np.float32,
         )
         for i, batch in enumerate(batch_handler.val_data):
-            exo_data = self.get_hr_exo_input(batch.high_res)
-            loss, _ = self.calc_loss(
+            logger.info(f'Calculating validation loss for batch {i} / '
+                        f'{len(batch_handler.val_data)}...')
+            loss, loss_details, _, _ = self._get_hr_exo_and_loss(
+                low_res=batch.low_res,
                 hi_res_true=batch.high_res,
-                hi_res_gen=self._tf_generate(batch.low_res, exo_data),
                 weight_gen_advers=weight_gen_advers,
-                train_gen=True,
-                train_disc=True,
             )
             row = i // batch_handler.n_time_bins
             col = i % batch_handler.n_time_bins
-            losses[row, col] = loss
-        return losses
-
-    def calc_val_loss_gen_content(self, batch_handler):
-        """Calculate the validation content loss across the validation
-        samples. e.g. If the sample domain has 100 steps and the
-        validation set has 10 bins then this will get a list of losses across
-        step 0 to 10, 10 to 20, etc.  Use this to determine performance
-        within bins and to update how observations are selected from these
-        bins.
-
-        Parameters
-        ----------
-        batch_handler : sup3r.preprocessing.BatchHandlerDC
-            BatchHandler object to iterate through
-
-        Returns
-        -------
-        list
-            List of content losses for all sample bins
-        """
-        losses = np.zeros(
-            (batch_handler.n_space_bins, batch_handler.n_time_bins),
-            dtype=np.float32,
-        )
-        for i, batch in enumerate(batch_handler.val_data):
-            exo_data = self.get_hr_exo_input(batch.high_res)
-            loss, _ = self.calc_loss_gen_content(
-                hi_res_true=batch.high_res,
-                hi_res_gen=self._tf_generate(batch.low_res, exo_data),
-            )
-            row = i // batch_handler.n_time_bins
-            col = i % batch_handler.n_time_bins
-            losses[row, col] = loss
-        return losses
+            total_losses[row, col] = loss
+            content_losses[row, col] = loss_details['loss_gen_content']
+        return total_losses, content_losses
 
     def calc_val_loss(self, batch_handler, weight_gen_advers):
         """Overloading the base calc_val_loss method. Method updates the
@@ -107,9 +81,11 @@ class Sup3rGanDC(Sup3rGan):
             Updated loss_details with mean validation loss calculated using
             the validation samples across the time bins
         """
+        logger.debug('Starting end-of-epoch validation loss calculation...')
         loss_details = {}
-        total_losses = self.calc_val_loss_gen(batch_handler, weight_gen_advers)
-        content_losses = self.calc_val_loss_gen_content(batch_handler)
+        total_losses, content_losses = self.calc_val_loss_gen(
+            batch_handler, weight_gen_advers
+        )
 
         t_weights = total_losses.mean(axis=0)
         t_weights /= t_weights.sum()
