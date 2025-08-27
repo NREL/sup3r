fix: optimize loss calculation and update test index selection methods

sup3r/models/with_obs.py
@@ -82,21 +82,17 @@ class Sup3rGanWithObs(Sup3rGan):
         self.loss_obs_fun = self.get_loss_fun(loss_obs)
         self.loss_obs_weight = loss_obs_weight
 
-    # @tf.function
+    @tf.function
     def _get_loss_obs_comparison(self, hi_res_true, hi_res_gen, obs_mask):
         """Get loss for observation locations and for non observation
         locations."""
-        hr_true = tf.stack(
-            [hi_res_true[..., idx] for idx in self.obs_training_inds],
-            axis=-1,
+        hr_true = hi_res_true[..., : len(self.hr_out_features)]
+        loss_obs, _ = self.loss_obs_fun(
+            hi_res_gen[~obs_mask], hr_true[~obs_mask]
         )
-        hr_gen = tf.stack(
-            [hi_res_gen[..., idx] for idx in self.obs_training_inds],
-            axis=-1,
+        loss_non_obs, _ = self.loss_obs_fun(
+            hi_res_gen[obs_mask], hr_true[obs_mask]
         )
-        mask = obs_mask[..., : hr_gen.shape[-1]]
-        loss_obs, _ = self.loss_obs_fun(hr_gen[~mask], hr_true[~mask])
-        loss_non_obs, _ = self.loss_obs_fun(hr_gen[mask], hr_true[mask])
         return loss_obs, loss_non_obs
 
     @property

tests/rasterizers/test_exo.py
@@ -393,7 +393,7 @@ def test_obs_nn(s_enhance, with_nans):
             d_i = dists[nn == i]
             if d_i.size == 0:
                 continue
-            idx = np.argmin(np.abs(d_i.min() - dists))
+            idx = np.abs(d_i.min() - dists).idxmin()
             true_obs[i, :] = vals[idx, :]
 
         mask = np.isnan(agg_obs) | np.isnan(true_obs)
@@ -467,7 +467,7 @@ def test_obs_idw(s_enhance, with_nans):
                 continue
             v_i = []
             for d in d_i:
-                idx = np.argmin(np.abs(d - dists))
+                idx = np.abs(d - dists).idxmin()
                 v_i.append(vals[idx, :])
             w_i = 1 / np.maximum(d_i, 1e-6)
             w_i /= np.sum(w_i)